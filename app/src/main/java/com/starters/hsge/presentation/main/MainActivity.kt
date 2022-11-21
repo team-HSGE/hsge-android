c505c4d588ec1244992c86c5b8aec9a78b592738
seoyoon513 <syoonlee513@gmail.com>
2022-11-21 21:38:21 +0900

    [CHORE] #1 create layer directory

--- app/src/main/java/com/starters/hsge/presentation/main/MainActivity.kt
+++ app/src/main/java/com/starters/hsge/presentation/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.starters.hsge
+package com.starters.hsge.presentation.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.starters.hsge.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
