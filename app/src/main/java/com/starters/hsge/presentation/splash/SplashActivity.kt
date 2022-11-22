ace98f405778f7a49e00fcbfba0bb394f9505f22
seoyoon513 <syoonlee513@gmail.com>
2022-11-22 15:45:36 +0900

    [ADD] #1 add SplashActivity

---
+++ app/src/main/java/com/starters/hsge/presentation/splash/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.starters.hsge.presentation.splash
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.starters.hsge.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
