f73e2a1860f273f7fdce289a3e9d72e11a5b635c
syb8200 <syb8200@naver.com>
2022-11-22 18:08:11 +0900

    [FEAT] #4 splash 구현

--- app/src/main/java/com/starters/hsge/presentation/splash/SplashActivity.kt
+++ app/src/main/java/com/starters/hsge/presentation/splash/SplashActivity.kt
@@ -1,13 +1,30 @@
 package com.starters.hsge.presentation.splash
 
 import android.annotation.SuppressLint
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.starters.hsge.R
+import com.starters.hsge.presentation.login.LoginActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }, DURATION)
+
+        window.statusBarColor = Color.parseColor("#FFBF43")
     }
+
+    companion object {
+        private const val DURATION : Long = 3000
+    }
+
 }
