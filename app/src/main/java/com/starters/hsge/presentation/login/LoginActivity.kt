46ce6e02663507888d4ccd48bb81fb01dddf6270
seoyoon513 <syoonlee513@gmail.com>
2022-11-22 15:42:29 +0900

    [ADD] #1 add LoginActivity

---
+++ app/src/main/java/com/starters/hsge/presentation/login/LoginActivity.kt
@@ -0,0 +1,14 @@
+package com.starters.hsge.presentation.login
+
+import android.os.Bundle
+import com.starters.hsge.R
+import com.starters.hsge.databinding.ActivityLoginBinding
+import com.starters.hsge.presentation.common.base.BaseActivity
+
+class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
