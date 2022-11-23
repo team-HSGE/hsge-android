f8d35471fffc1ce6d6e3d367498176df88706c71
seoyoon513 <syoonlee513@gmail.com>
2022-11-23 18:45:14 +0900

    [FEAT] #3 navigation 연결

--- app/src/main/java/com/starters/hsge/presentation/register/DogPhotoFragment.kt
+++ app/src/main/java/com/starters/hsge/presentation/register/DogPhotoFragment.kt
@@ -2,6 +2,7 @@ package com.starters.hsge.presentation.register
 
 import android.os.Bundle
 import android.view.View
+import androidx.navigation.Navigation
 import com.starters.hsge.R
 import com.starters.hsge.databinding.FragmentDogPhotoBinding
 import com.starters.hsge.presentation.common.base.BaseFragment
@@ -10,5 +11,16 @@ class DogPhotoFragment : BaseFragment<FragmentDogPhotoBinding>(R.layout.fragment
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        initListener()
+    }
+
+    private fun initListener() {
+
+        binding.tvNextButton.setOnClickListener {
+            Navigation.findNavController(binding.root)
+                .navigate(R.id.action_dogPhotoFragment_to_userLocationFragment)
+
+        }
     }
 }
