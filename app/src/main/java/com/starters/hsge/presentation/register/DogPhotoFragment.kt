4c1f757015c99a75d2b20cb5481314c893a46739
seoyoon513 <syoonlee513@gmail.com>
2022-11-23 18:42:58 +0900

    [ADD] #3 add DogPhotoFragment

---
+++ app/src/main/java/com/starters/hsge/presentation/register/DogPhotoFragment.kt
@@ -0,0 +1,14 @@
+package com.starters.hsge.presentation.register
+
+import android.os.Bundle
+import android.view.View
+import com.starters.hsge.R
+import com.starters.hsge.databinding.FragmentDogPhotoBinding
+import com.starters.hsge.presentation.common.base.BaseFragment
+
+class DogPhotoFragment : BaseFragment<FragmentDogPhotoBinding>(R.layout.fragment_dog_photo) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
