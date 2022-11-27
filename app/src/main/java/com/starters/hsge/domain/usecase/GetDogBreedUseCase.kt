2424511946f5aab05fb80b13d591f6b720df424a
seoyoon513 <syoonlee513@gmail.com>
2022-11-27 09:31:42 +0900

    [REFACTOR] #12 usecase추가 & view모델 연결

---
+++ app/src/main/java/com/starters/hsge/domain/usecase/GetDogBreedUseCase.kt
@@ -0,0 +1,10 @@
+package com.starters.hsge.domain.usecase
+
+import com.starters.hsge.domain.repository.DogProfileRepository
+import javax.inject.Inject
+
+class GetDogBreedUseCase @Inject constructor(
+    private val dogProfileRepository: DogProfileRepository
+) {
+    operator fun invoke() = dogProfileRepository.getDogBreed()
+}
