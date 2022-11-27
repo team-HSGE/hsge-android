d47c83c0433f045b881c844ed9c97f6ceda72d6e
seoyoon513 <syoonlee513@gmail.com>
2022-11-27 17:45:14 +0900

    [CHORE] #16 필요없는 코드 정리

--- app/src/main/java/com/starters/hsge/presentation/register/fragment/DogAgeFragment.kt
+++ app/src/main/java/com/starters/hsge/presentation/register/fragment/DogAgeFragment.kt
@@ -20,15 +20,6 @@ class DogAgeFragment : BaseFragment<FragmentDogAgeBinding>(R.layout.fragment_dog
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-//        val dogAgeList = listOf<String>(
-//            "1개월 미만", "1개월", "2개월", "3개월", "4개월", "5개월",
-//            "6개월", "7개월", "8개월", "9개월", "10개월", "11개월",
-//            "1살", "2살", "3살", "4살", "5살", "6살", "7살", "8살",
-//            "9살", "10살", "11살", , "13살", "14살", "15살", "16살",
-//            "17살", "18살", "19살", "20살", "20살 이상"
-//        )
-
-
         initListener()
     }
 
