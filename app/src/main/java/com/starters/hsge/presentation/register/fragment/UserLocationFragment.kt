7f405b141ef0da419297f86401af1a3be88d74ca
안정은 <jjeee1313@gmail.com>
2022-11-28 20:06:01 +0900

    [FEAT] #6  내 위치 받아와서 도로명 변환

--- app/src/main/java/com/starters/hsge/presentation/register/fragment/UserLocationFragment.kt
+++ app/src/main/java/com/starters/hsge/presentation/register/fragment/UserLocationFragment.kt
@@ -1,11 +1,17 @@
 package com.starters.hsge.presentation.register.fragment
 
 import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Geocoder
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.navigation.Navigation
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.starters.hsge.R
 import com.starters.hsge.databinding.FragmentUserLocationBinding
 import com.starters.hsge.presentation.common.base.BaseFragment
@@ -14,6 +20,8 @@ class UserLocationFragment :
     BaseFragment<FragmentUserLocationBinding>(R.layout.fragment_user_location) {
 
     private lateinit var locationPermissionRequest: ActivityResultLauncher<Array<String>>
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -41,17 +49,56 @@ class UserLocationFragment :
     private fun initListener() {
         // 시스템 권한 대화상자 요청
         binding.btnSearch.setOnClickListener {
+            checkLocationPermission()
+//            locationPermissionRequest.launch(
+//                arrayOf(
+//                    Manifest.permission.ACCESS_FINE_LOCATION,
+//                    Manifest.permission.ACCESS_COARSE_LOCATION
+//                )
+//            )
+        }
+
+        binding.btnNext.setOnClickListener {
+            Navigation.findNavController(binding.root)
+                .navigate(R.id.action_userLocationFragment_to_userDistanceFragment)
+        }
+    }
+
+
+    private fun checkLocationPermission() {
+        val fineLocationGranted = ContextCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+        val coarseLocationGranted = ContextCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+
+        if (!fineLocationGranted && !coarseLocationGranted) {
+
+            Toast.makeText(context, "$fineLocationGranted + if", Toast.LENGTH_SHORT).show()
+
             locationPermissionRequest.launch(
                 arrayOf(
                     Manifest.permission.ACCESS_FINE_LOCATION,
                     Manifest.permission.ACCESS_COARSE_LOCATION
                 )
             )
-        }
-
-        binding.btnNext.setOnClickListener {
-            Navigation.findNavController(binding.root)
-                .navigate(R.id.action_userLocationFragment_to_userDistanceFragment)
+        } else {
+            val geocoder = Geocoder(requireContext())
+            fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
+            fusedLocationClient.lastLocation.addOnSuccessListener {
+                if (it != null) {
+                    val address = geocoder.getFromLocation(it.latitude, it.longitude, 1)
+                    val korAddress = address?.get(0)
+                    val location =
+                        korAddress?.adminArea + " " + korAddress?.subLocality + " " + korAddress?.thoroughfare
+                    binding.tvMyLocation.text = location
+                } else {
+                    Toast.makeText(requireContext(), "Location이 null임", Toast.LENGTH_SHORT).show()
+                }
+            }
         }
     }
 }
