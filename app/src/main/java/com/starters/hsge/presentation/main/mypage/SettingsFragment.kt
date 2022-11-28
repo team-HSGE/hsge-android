e5e7fca7099fa92a318def7bff94807fe576271a
syb8200 <syb8200@naver.com>
2022-11-28 14:01:24 +0900

    [UI] #13 설정 화면 작성

---
+++ app/src/main/java/com/starters/hsge/presentation/main/mypage/SettingsFragment.kt
@@ -0,0 +1,14 @@
+package com.starters.hsge.presentation.main.mypage
+
+import android.os.Bundle
+import android.view.View
+import com.starters.hsge.R
+import com.starters.hsge.databinding.FragmentSettingsBinding
+import com.starters.hsge.presentation.common.base.BaseFragment
+
+class SettingsFragment : BaseFragment<FragmentSettingsBinding>(R.layout.fragment_settings) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
