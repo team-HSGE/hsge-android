7b3a746781ec7dd3e8294d1361931f7b43ac791d
안정은 <jjeee1313@gmail.com>
2022-11-29 11:39:51 +0900

    [FEAT] #6 내 위치 받아오기 수정

--- app/src/main/java/com/starters/hsge/presentation/register/UserLocationFragment.kt
+++ app/src/main/java/com/starters/hsge/presentation/register/UserLocationFragment.kt
@@ -3,15 +3,19 @@ package com.starters.hsge.presentation.register
 import android.Manifest
 import android.content.pm.PackageManager
 import android.location.Geocoder
+import android.location.LocationRequest
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
 import androidx.navigation.Navigation
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.LocationSettingsRequest
 import com.starters.hsge.R
 import com.starters.hsge.databinding.FragmentUserLocationBinding
 import com.starters.hsge.presentation.common.base.BaseFragment
@@ -23,6 +27,7 @@ class UserLocationFragment :
     private lateinit var fusedLocationClient: FusedLocationProviderClient
 
 
+    @RequiresApi(Build.VERSION_CODES.S)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -46,26 +51,19 @@ class UserLocationFragment :
 
     }
 
+    @RequiresApi(Build.VERSION_CODES.S)
     private fun initListener() {
-        // 시스템 권한 대화상자 요청
         binding.btnSearch.setOnClickListener {
             checkLocationPermission()
-//            locationPermissionRequest.launch(
-//                arrayOf(
-//                    Manifest.permission.ACCESS_FINE_LOCATION,
-//                    Manifest.permission.ACCESS_COARSE_LOCATION
-//                )
-//            )
         }
-
         binding.btnNext.setOnClickListener {
             Navigation.findNavController(binding.root)
                 .navigate(R.id.action_userLocationFragment_to_radiusFragment)
-
         }
     }
 
 
+    @RequiresApi(Build.VERSION_CODES.S)
     private fun checkLocationPermission() {
         val fineLocationGranted = ContextCompat.checkSelfPermission(
             requireContext(),
@@ -77,9 +75,7 @@ class UserLocationFragment :
         ) == PackageManager.PERMISSION_GRANTED
 
         if (!fineLocationGranted && !coarseLocationGranted) {
-
-            Toast.makeText(context, "$fineLocationGranted + if", Toast.LENGTH_SHORT).show()
-
+            // 권한이 없다면 시스템 권한 대화상자 요청
             locationPermissionRequest.launch(
                 arrayOf(
                     Manifest.permission.ACCESS_FINE_LOCATION,
@@ -87,22 +83,53 @@ class UserLocationFragment :
                 )
             )
         } else {
+            // 권한이 있으면 현재 위치 도로명 주소로 변환
             val geocoder = Geocoder(requireContext())
             fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
-            fusedLocationClient.lastLocation.addOnSuccessListener {
-                if (it != null) {
-                    val address = geocoder.getFromLocation(it.latitude, it.longitude, 1)
 
-                    val latitude = it.latitude
-                    val longitude = it.longitude
+            // LocationRequest 생성
 
-                    val korAddress = address?.get(0)
-                    val location =
-                        korAddress?.adminArea + " " + korAddress?.subLocality + " " + korAddress?.thoroughfare
-                    binding.tvMyLocation.text = location
-                } else {
-                    Toast.makeText(requireContext(), "Location이 null임", Toast.LENGTH_SHORT).show()
+            val locationRequest = com.google.android.gms.location.LocationRequest.create().apply {
+                interval = 1000
+                priority = com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY
+            }
+
+            val builder = LocationSettingsRequest.Builder().addLocationRequest(locationRequest)
+            val client = LocationServices.getSettingsClient(requireContext())
+            val task = client.checkLocationSettings(builder.build())
+
+            task.addOnSuccessListener {
+                //Toast.makeText(requireContext(), "location client setting success", Toast.LENGTH_SHORT).show()
+                fusedLocationClient.lastLocation.addOnSuccessListener {
+                    if (it != null) {
+                        val address = geocoder.getFromLocation(it.latitude, it.longitude, 1)
+
+                        val latitude = it.latitude
+                        val longitude = it.longitude
+
+                        val addressLine = address?.get(0)?.getAddressLine(0)
+                        val addressList = addressLine?.split(" ") as ArrayList<String>
+                        addressList.removeAt(0)
+                        addressList.removeAt(addressList.size - 1)
+
+                        val locationAddress = StringBuilder()
+                        for (i in addressList) {
+                            locationAddress.append(i)
+                            locationAddress.append(" ")
+                        }
+
+                        Toast.makeText(requireContext(), "$locationAddress", Toast.LENGTH_SHORT)
+                            .show()
+                        binding.tvMyLocation.text = locationAddress
+
+                    } else {
+                        Toast.makeText(requireContext(), "Location이 null임", Toast.LENGTH_SHORT)
+                            .show()
+                    }
                 }
+            }
+            task.addOnFailureListener {
+                Toast.makeText(requireContext(), "location client setting failure", Toast.LENGTH_SHORT).show()
             }
         }
     }
