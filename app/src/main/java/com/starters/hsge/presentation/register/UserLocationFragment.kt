b1e4e0c1fcd2a201ad7a5bace4f904554cca8334
안정은 <jjeee1313@gmail.com>
2022-11-30 15:43:11 +0900

    [FEAT] #6 주소가 textView에 뜰 때까지 LoadingDialog 띄우기

--- app/src/main/java/com/starters/hsge/presentation/register/UserLocationFragment.kt
+++ app/src/main/java/com/starters/hsge/presentation/register/UserLocationFragment.kt
@@ -43,6 +43,7 @@ class UserLocationFragment :
             if (isNetworkAvailable(requireContext()) && isEnableLocationSystem(requireContext())) {
                 if (checkPermissionForLocation(requireContext())) {
                     startLocationUpdates()
+                    showLoadingDialog(requireContext())
                 }
             } else if (!isNetworkAvailable(requireContext())) { // 네트워크 연결 안 됨
                 binding.tvMyLocation.hint = "ex) 서울특별시 중구 무교동"
@@ -61,6 +62,58 @@ class UserLocationFragment :
                 .navigate(R.id.action_userLocationFragment_to_radiusFragment)
         }
     }
+
+    // 네트워크 상태 체크
+    private fun isNetworkAvailable(context: Context): Boolean {
+        val connectManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkStatus: NetworkInfo? = connectManager.activeNetworkInfo
+        return networkStatus?.isConnectedOrConnecting == true
+    }
+
+
+    // 기기 위치 정보 켜져있는지 여부 확인
+    private fun isEnableLocationSystem(context: Context): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            val locationManager =
+                context.getSystemService(Context.LOCATION_SERVICE) as? LocationManager
+            locationManager!!.isLocationEnabled
+        } else {
+            val mode = Settings.Secure.getInt(
+                context.contentResolver,
+                Settings.Secure.LOCATION_MODE,
+                Settings.Secure.LOCATION_MODE_OFF
+            )
+            mode != Settings.Secure.LOCATION_MODE_OFF
+        }
+    }
+
+
+    // 권한 체크
+    private fun checkPermissionForLocation(context: Context): Boolean {
+        // Android 6.0 Marshmallow 이상에서는 위치 권한에 추가 런타임 권한이 필요
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+            if (context.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) { // 위치 권한 허용되어있는 경우
+                true
+            } else if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)){ // 위치 권한 거부되어있는 경우
+                // 권한이 없으므로 권한 요청 알림 보내기
+                ActivityCompat.requestPermissions(
+                    requireActivity(),
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    REQUEST_PERMISSION_LOCATION
+                )
+                false
+            }
+            else{ // 위치 권한 거부 및 다시 묻지 않음인 경우
+                Toast.makeText(requireContext(), "위치 권한이 없어 해당 기능을 실행할 수 없습니다.", Toast.LENGTH_SHORT).show()
+                false
+            }
+        } else {
+            true
+        }
+    }
+
 
     // 위치 갱신
     private fun startLocationUpdates() {
@@ -98,68 +151,6 @@ class UserLocationFragment :
             }
     }
 
-    // 권한 체크
-    private fun checkPermissionForLocation(context: Context): Boolean {
-        // Android 6.0 Marshmallow 이상에서는 위치 권한에 추가 런타임 권한이 필요
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-
-            if (context.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) { // 위치 권한 허용되어있는 경우
-                true
-            } else if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)){ // 위치 권한 거부되어있는 경우
-                // 권한이 없으므로 권한 요청 알림 보내기
-                ActivityCompat.requestPermissions(
-                    requireActivity(),
-                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                    REQUEST_PERMISSION_LOCATION
-                )
-                false
-            }
-            else{ // 위치 권한 거부 및 다시 묻지 않음인 경우
-                Toast.makeText(requireContext(), "위치 권한이 없어 해당 기능을 실행할 수 없습니다.", Toast.LENGTH_SHORT).show()
-                false
-            }
-        } else {
-            true
-        }
-    }
-
-    // 기기 위치 정보 켜져있는지 여부 확인
-    private fun isEnableLocationSystem(context: Context): Boolean {
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-            val locationManager =
-                context.getSystemService(Context.LOCATION_SERVICE) as? LocationManager
-            locationManager!!.isLocationEnabled
-        } else {
-            val mode = Settings.Secure.getInt(
-                context.contentResolver,
-                Settings.Secure.LOCATION_MODE,
-                Settings.Secure.LOCATION_MODE_OFF
-            )
-            mode != Settings.Secure.LOCATION_MODE_OFF
-        }
-    }
-
-    // 네트워크 상태 체크
-    private fun isNetworkAvailable(context: Context): Boolean {
-        val connectManager =
-            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val networkStatus: NetworkInfo? = connectManager.activeNetworkInfo
-        return networkStatus?.isConnectedOrConnecting == true
-    }
-
-    // 화면 회전 시 데이터 할당
-    private fun rotationKeep(savedInstanceState: Bundle?) {
-        if (savedInstanceState != null) {
-            mLocation = savedInstanceState.getString("location").toString()
-            binding.tvMyLocation.text = mLocation
-        }
-    }
-
-    // 텍스트 저장(화면 회전 시 유지 위함)
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putString("location", binding.tvMyLocation.text.toString())
-    }
 
     // 위경도 -> geocoder
     private fun convertToAddress(geocoder: Geocoder, it: Location) {
@@ -176,7 +167,27 @@ class UserLocationFragment :
             locationAddress.append(" ")
         }
         binding.tvMyLocation.text = locationAddress
+        dismissLoadingDialog()
     }
+
+
+    // 화면 회전 시 데이터 할당
+    private fun rotationKeep(savedInstanceState: Bundle?) {
+        if (savedInstanceState != null) {
+            mLocation = savedInstanceState.getString("location").toString()
+            binding.tvMyLocation.text = mLocation
+        }
+    }
+
+
+    // 텍스트 저장(화면 회전 시 유지 위함)
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("location", binding.tvMyLocation.text.toString())
+    }
+
+
+
 
 
 }
