2e977991f8ddaa956a00e06a8c9c0a48b201e24f
안정은 <jjeee1313@gmail.com>
2022-11-30 15:39:26 +0900

    [FEAT] #6 BaseFragment랑 Commin-Util에 다이얼로그 추가

--- app/src/main/java/com/starters/hsge/presentation/common/base/BaseFragment.kt
+++ app/src/main/java/com/starters/hsge/presentation/common/base/BaseFragment.kt
@@ -1,5 +1,6 @@
 package com.starters.hsge.presentation.common.base
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,12 +9,14 @@ import androidx.annotation.LayoutRes
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
+import com.starters.hsge.presentation.common.util.LoadingDialog
 
 abstract class BaseFragment<T: ViewDataBinding>(
     @LayoutRes val layoutResId: Int
 ) : Fragment(){
     private var _binding: T? = null
     protected val binding get() = _binding!!
+    lateinit var mLoadingDialog : LoadingDialog
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,5 +36,17 @@ abstract class BaseFragment<T: ViewDataBinding>(
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    fun showLoadingDialog(context: Context){
+        mLoadingDialog = LoadingDialog(context)
+        mLoadingDialog.show()
+    }
+
+    // 띄워 놓은 로딩 다이얼로그 없앰
+    fun dismissLoadingDialog() {
+        if(mLoadingDialog.isShowing){
+            mLoadingDialog.dismiss()
+        }
     }
 }
