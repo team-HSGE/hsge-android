30dc5973c16bb015272a4332d229d8e52fcfed97
seoyoon513 <syoonlee513@gmail.com>
2022-11-30 10:42:08 +0900

    [FEAT] #16 강아지 성별 저장 & 버튼 활성화

--- app/src/main/java/com/starters/hsge/presentation/register/fragment/DogSexFragment.kt
+++ app/src/main/java/com/starters/hsge/presentation/register/fragment/DogSexFragment.kt
@@ -2,34 +2,52 @@ package com.starters.hsge.presentation.register.fragment
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.viewModels
 import androidx.navigation.Navigation
 import com.starters.hsge.R
 import com.starters.hsge.databinding.FragmentDogSexBinding
 import com.starters.hsge.presentation.common.base.BaseFragment
+import com.starters.hsge.presentation.register.viewmodel.RegisterViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class DogSexFragment : BaseFragment<FragmentDogSexBinding>(R.layout.fragment_dog_sex) {
+
+    private val registerViewModel: RegisterViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         initListener()
+
     }
 
     private fun initListener() {
 
-        binding.btnMale.setOnClickListener {
-            if (binding.btnMale.isSelected) {
-                !binding.btnMale.isSelected
+        binding.radioGroup.setOnCheckedChangeListener { _, checkedId ->
+            when(checkedId) {
+                R.id.rbtn_male -> {
+                    registerViewModel.dogSex = "남자"
+                    setButtonEnable()
+                }
+                R.id.rbtn_female -> {
+                    registerViewModel.dogSex = "여자"
+                    setButtonEnable() // 멘토님꼐 물어보기 ^__^
+                }
             }
         }
 
-        binding.btnFemale.setOnClickListener {
-
+        binding.checkboxNeuter.setOnCheckedChangeListener { _, isChecked ->
+            registerViewModel.dogNeuter = isChecked
         }
 
         binding.btnNext.setOnClickListener {
             Navigation.findNavController(binding.root)
                 .navigate(R.id.action_dogSexFragment_to_dogAgeFragment)
         }
+    }
+
+    private fun setButtonEnable() {
+        binding.btnNext.isEnabled = !registerViewModel.dogSex.isNullOrEmpty()
     }
 }
