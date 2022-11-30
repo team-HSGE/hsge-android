a66ec8e1a3f52c20a534fe186d97dd7f22087951
안정은 <jjeee1313@gmail.com>
2022-11-30 17:44:42 +0900

    [UI] #14 카드 overlay 설정

--- app/src/main/java/com/starters/hsge/presentation/main/home/HomeFragment.kt
+++ app/src/main/java/com/starters/hsge/presentation/main/home/HomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.animation.AccelerateInterpolator
 import com.starters.hsge.R
 import com.starters.hsge.data.model.DogCard
+import com.starters.hsge.data.model.Tag
 import com.starters.hsge.databinding.FragmentHomeBinding
 import com.starters.hsge.presentation.common.base.BaseFragment
 import com.starters.hsge.presentation.main.home.adapter.CardStackAdapter
@@ -45,18 +46,23 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
 
         })
 
-//        val dogCardList = listOf<DogCard>(
-//            DogCard(
-//                "뽀삐",
-//                "https://user-images.githubusercontent.com/106398273/204183502-2974fadc-dae6-4238-83ce-3b4acd12373d.jpeg",
-//                "스탠다드 푸들",
-//                " I 12살 10개월 I ",
-//                "Female",
-//                true,
-//                Tag(listOf("고구마", "간식", "수영"), listOf("산책", "카메라"))
-//            )
-//        )
+        val dogCardList = listOf<DogCard>(
+            DogCard(
+                "뽀삐",
+                "https://user-images.githubusercontent.com/106398273/204183502-2974fadc-dae6-4238-83ce-3b4acd12373d.jpeg",
+                "스탠다드 푸들",
+                " I 12살 10개월 I ",
+                "Female",
+                true,
+                Tag(listOf("고구마", "간식", "수영"), listOf("산책", "카메라"))
+            )
+        )
 
+        // 로컬 데이터
+        cardStackAdapter = CardStackAdapter(requireContext(), dogCardList)
+        manager.setSwipeableMethod(SwipeableMethod.AutomaticAndManual)
+        binding.cardStackView.layoutManager = manager
+        binding.cardStackView.adapter = cardStackAdapter
 
 
         fabClick(binding.cardStackView)
@@ -98,10 +104,10 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
                     Log.d("TAG", response.toString())
                     Log.d("TAG", "성공")
                     val dogCardResult = response.body()
-                    cardStackAdapter = CardStackAdapter(requireContext(), dogCardResult!!)
-                    manager.setSwipeableMethod(SwipeableMethod.AutomaticAndManual)
-                    binding.cardStackView.layoutManager = manager
-                    binding.cardStackView.adapter = cardStackAdapter
+//                    cardStackAdapter = CardStackAdapter(requireContext(), dogCardResult!!)
+//                    manager.setSwipeableMethod(SwipeableMethod.AutomaticAndManual)
+//                    binding.cardStackView.layoutManager = manager
+//                    binding.cardStackView.adapter = cardStackAdapter
                 } else{
                     Log.d("TAG", "실패")
                     Log.d("TAG", response.code().toString())
