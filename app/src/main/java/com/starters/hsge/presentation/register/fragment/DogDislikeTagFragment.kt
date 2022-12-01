ae16dc0ac9bb7a3997e92c0ff0240953b13881e0
seoyoon513 <syoonlee513@gmail.com>
2022-12-01 20:43:53 +0900

    [FEAT] #16 like chip 동적 추가

--- app/src/main/java/com/starters/hsge/presentation/register/fragment/DogDislikeTagFragment.kt
+++ app/src/main/java/com/starters/hsge/presentation/register/fragment/DogDislikeTagFragment.kt
@@ -3,28 +3,36 @@ package com.starters.hsge.presentation.register.fragment
 import android.os.Bundle
 import android.view.View
 import androidx.navigation.Navigation
+import com.google.android.material.chip.Chip
 import com.starters.hsge.R
 import com.starters.hsge.databinding.FragmentDogDislikeTagBinding
 import com.starters.hsge.presentation.common.base.BaseFragment
 
 class DogDislikeTagFragment : BaseFragment<FragmentDogDislikeTagBinding>(R.layout.fragment_dog_dislike_tag) {
 
+    lateinit var chip: Chip
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val list = listOf(
+            "#남자사람", "#여자사람", "#아이", "#사람", "#암컷", "#대형견", "#중형견",
+            "#소형견", "#옷입기", "#사진찍기", "#수영", "#뽀뽀", "#발만지기", "#꼬리만지기",
+            "#스킨십", "#큰소리", "#향수")
+
         initListener()
+        initChipButton(list)
+    }
+
+    private fun initChipButton(chipList: List<String>) {
+        for (i in chipList) {
+            chip = Chip(context, null, R.attr.CustomDislikeChipChoiceStyle)
+            chip.text = i
+            binding.chipGroupDislike.addView(chip)
+        }
     }
 
     private fun initListener() {
-        // chip click listener
-        binding.chipMan.setOnCheckedChangeListener { chip, isChecked ->
-            if (!isChecked) {
-                chip.text
-            } else {
-
-            }
-        }
-
         binding.btnNext.setOnClickListener {
             Navigation.findNavController(binding.root)
                 .navigate(R.id.action_dogDislikeTagFragment_to_userLocationFragment)
