d0c74b12d1d95e808a7a67584b89021a9acaa8b8
syb8200 <syb8200@naver.com>
2022-12-04 03:39:56 +0900

    [FEAT] #37 Android 12 대응 처리

--- app/src/main/java/com/starters/hsge/presentation/splash/SplashActivity.kt
+++ app/src/main/java/com/starters/hsge/presentation/splash/SplashActivity.kt
@@ -1,30 +1,80 @@
 package com.starters.hsge.presentation.splash
 
-import android.annotation.SuppressLint
 import android.content.Intent
-import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
+import android.view.View
+import android.view.ViewTreeObserver
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.starters.hsge.R
 import com.starters.hsge.presentation.login.LoginActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashActivity : AppCompatActivity() {
+
+    var isReady = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        initSplashScreen()
+    }
 
-        Handler(Looper.getMainLooper()).postDelayed({
-            startActivity(Intent(this, LoginActivity::class.java))
+    private fun initSplashScreen() {
+        initData()
+
+        // 31 버전일 떄와 아닐 때의 분기 처리
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+
+            val content : View = findViewById(android.R.id.content)
+            // SplashScreen이 생성되고 그려질 때 계속해서 호출된다.
+            content.viewTreeObserver.addOnPreDrawListener(
+                object : ViewTreeObserver.OnPreDrawListener {
+                    override fun onPreDraw(): Boolean {
+                        // Check if the initial data is ready.
+                        return if (isReady) {
+                            // 3초 후 Splash Screen 제거
+                            content.viewTreeObserver.removeOnPreDrawListener(this)
+                            true
+                        } else {
+                            // content is not ready
+                            false
+                        }
+                    }
+                }
+            )
+            startLoginActivity()
+        } else {
+            // 31 버전이 아닌 경우, 별도 처리.
+            setContentView(R.layout.activity_splash)
+            startLoginActivity()
+        }
+    }
+
+    private fun initData() {
+        // 별도의 데이터 처리가 없기 때문에 3초의 딜레이를 줌.
+        // 선행되어야 하는 작업이 있는 경우, 이곳에서 처리 후 isReady를 변경.
+        CoroutineScope(Dispatchers.IO).launch {
+            delay(DURATION)
+            isReady = true
+        }
+    }
+
+    private fun startLoginActivity() {
+        CoroutineScope(Dispatchers.IO).launch {
+            delay(DURATION)
+            startActivity(Intent(applicationContext, LoginActivity::class.java))
             finish()
-        }, DURATION)
-
-        window.statusBarColor = Color.parseColor("#FFBF43")
+        }
     }
 
     companion object {
-        private const val DURATION : Long = 3000
+        private const val DURATION : Long = 1000
     }
 
 }
+
