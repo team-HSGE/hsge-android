c78891bbe3a3027d51aa9bfcc9ee22fa699f81f6
seoyoon513 <syoonlee513@gmail.com>
2022-12-05 00:01:07 +0900

    [FEAT] #35 RepositoryModule에 RegisterRepository 추가

--- app/src/main/java/com/starters/hsge/di/RepositoryModule.kt
+++ app/src/main/java/com/starters/hsge/di/RepositoryModule.kt
@@ -2,8 +2,10 @@ package com.starters.hsge.di
 
 import com.starters.hsge.data.repository.DogOptionRepositoryImpl
 import com.starters.hsge.data.repository.DogProfileRepositoryImpl
+import com.starters.hsge.data.repository.RegisterRepositoryImpl
 import com.starters.hsge.domain.repository.DogOptionRepository
 import com.starters.hsge.domain.repository.DogProfileRepository
+import com.starters.hsge.domain.repository.RegisterRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -24,5 +26,11 @@ abstract class RepositoryModule {
     abstract fun bindsDogOptionRepository(
         dogOptionRepositoryImpl: DogOptionRepositoryImpl
     ): DogOptionRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindsRegisterRepository(
+        registerRepositoryImpl: RegisterRepositoryImpl
+    ): RegisterRepository
 
 }
