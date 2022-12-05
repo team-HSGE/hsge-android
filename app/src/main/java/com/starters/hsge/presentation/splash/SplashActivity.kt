f234f44bfd4b9d51ed9ef3a666b4844a808c4416
syb8200 <syb8200@naver.com>
2022-12-05 18:46:30 +0900

    [FEAT] #37 SharedPreference 오류 해결

--- app/src/main/java/com/starters/hsge/presentation/splash/SplashActivity.kt
+++ app/src/main/java/com/starters/hsge/presentation/splash/SplashActivity.kt
@@ -1,31 +1,43 @@
 package com.starters.hsge.presentation.splash
 
+import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.ViewTreeObserver
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.starters.hsge.R
+import com.starters.hsge.databinding.ActivityLoginBinding
+import com.starters.hsge.presentation.common.base.BaseActivity
+import com.starters.hsge.presentation.common.base.BaseActivity.Companion.prefs
 import com.starters.hsge.presentation.login.LoginActivity
+import com.starters.hsge.presentation.main.MainActivity
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_splash) {
 
     var isReady = false
+    var accessToken : String = ""
+    var refreshToken : String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
+
         initSplashScreen()
     }
 
     private fun initSplashScreen() {
         initData()
+        getSharedPreference()
 
         // 31 버전일 떄와 아닐 때의 분기 처리
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
@@ -37,7 +49,7 @@ class SplashActivity : AppCompatActivity() {
                     override fun onPreDraw(): Boolean {
                         // Check if the initial data is ready.
                         return if (isReady) {
-                            // 3초 후 Splash Screen 제거
+                            // Splash Screen 제거
                             content.viewTreeObserver.removeOnPreDrawListener(this)
                             true
                         } else {
@@ -47,12 +59,20 @@ class SplashActivity : AppCompatActivity() {
                     }
                 }
             )
-            startLoginActivity()
+            //startMainActivity()
+            checkToken()
         } else {
             // 31 버전이 아닌 경우, 별도 처리.
             setContentView(R.layout.activity_splash)
-            startLoginActivity()
+            checkToken()
+            //startMainActivity()
         }
+    }
+
+    private fun getSharedPreference() {
+        accessToken = prefs.getString("accessToken", "")!!
+        refreshToken = prefs.getString("refreshToken", "")!!
+        Log.d("sp", "access: ${accessToken}\nrefresh: ${refreshToken}")
     }
 
     private fun initData() {
@@ -64,10 +84,26 @@ class SplashActivity : AppCompatActivity() {
         }
     }
 
+    private fun checkToken() {
+        if (!prefs.contains("accessToken")){
+            startLoginActivity()
+        } else {
+            startMainActivity()
+        }
+    }
+
     private fun startLoginActivity() {
         CoroutineScope(Dispatchers.IO).launch {
             delay(DURATION)
             startActivity(Intent(applicationContext, LoginActivity::class.java))
+            finish()
+        }
+    }
+
+    private fun startMainActivity() {
+        CoroutineScope(Dispatchers.IO).launch {
+            delay(DURATION)
+            startActivity(Intent(applicationContext, MainActivity::class.java))
             finish()
         }
     }
