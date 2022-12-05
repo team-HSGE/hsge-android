cabb5decce267b33c501cdb68cd684013c8c7f3a
seoyoon513 <syoonlee513@gmail.com>
2022-12-05 15:09:08 +0900

    [FEAT] #35 RegisterInfo SerialName 추가

--- app/src/main/java/com/starters/hsge/domain/model/RegisterInfo.kt
+++ app/src/main/java/com/starters/hsge/domain/model/RegisterInfo.kt
@@ -1,20 +1,23 @@
 package com.starters.hsge.domain.model
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class RegisterInfo(
-    val email: String,
-    val userNickName: String,
-    val userIcon: Int,
-    val dogName: String,
-    val dogAge: String,
-    val dogSex: String,
-    val dogNeuter: Boolean = false,
-    val dogBreed: String,
-    val dogLikeTag: String,
-    val dogDislikeTag: String,
-    val latitude: Double,
-    val longitude: Double
+    @SerialName("email") val email: String,
+    @SerialName("nickname") val userNickName: String,
+    @SerialName("profilePath") val userIcon: Int,
+    @SerialName("petName") val dogName: String,
+    @SerialName("age") val dogAge: String,
+    @SerialName("gender") val dogSex: String,
+    @SerialName("neutralization") val dogNeuter: Boolean = false,
+    @SerialName("description") val description: String = "",
+    @SerialName("picture") val picture: String = "",
+    @SerialName("breed") val dogBreed: String,
+    @SerialName("likeTag") val dogLikeTag: String,
+    @SerialName("dislikeTag") val dogDislikeTag: String,
+    @SerialName("latitude") val latitude: Double,
+    @SerialName("longtitude") val longitude: Double
 )
 
