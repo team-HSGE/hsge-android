0682d3b19d54b0b4157d700eb874f17cdc9675cc
seoyoon513 <syoonlee513@gmail.com>
2022-12-05 15:09:44 +0900

    [FEAT] #35 데이터 변환 처리 위치 변경

--- app/src/main/java/com/starters/hsge/domain/repository/RegisterRepository.kt
+++ app/src/main/java/com/starters/hsge/domain/repository/RegisterRepository.kt
@@ -1,7 +1,8 @@
 package com.starters.hsge.domain.repository
 
+import com.starters.hsge.domain.model.RegisterInfo
 import java.io.File
 
 interface RegisterRepository {
-    suspend fun postUserData(img: File, data: String)
+    suspend fun postUserData(img: File, data: RegisterInfo)
 }
--- app/src/main/java/com/starters/hsge/domain/usecase/PostRegisterUseCase.kt
+++ app/src/main/java/com/starters/hsge/domain/usecase/PostRegisterUseCase.kt
@@ -1,19 +1,13 @@
 package com.starters.hsge.domain.usecase
 
-import android.net.Uri
-import androidx.core.net.toFile
 import com.starters.hsge.domain.model.RegisterInfo
 import com.starters.hsge.domain.repository.RegisterRepository
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
+import java.io.File
 import javax.inject.Inject
 
 class PostRegisterUseCase @Inject constructor(
     private val registerRepository: RegisterRepository
 ) {
-    suspend operator fun invoke(img: Uri, data: RegisterInfo) {
-        val formFile = img.toFile()
-        val formJson = Json.encodeToString(data)
-        registerRepository.postUserData(formFile, formJson)
-    }
+    suspend operator fun invoke(img: File, data: RegisterInfo) =
+        registerRepository.postUserData(img, data)
 }
