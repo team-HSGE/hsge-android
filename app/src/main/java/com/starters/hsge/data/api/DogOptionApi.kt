826e3819ad46314954b82448ccd2f738c76770c4
seoyoon513 <syoonlee513@gmail.com>
2022-12-05 11:54:39 +0900

    [FEAT] #35 breed 가져오는 api endpoint 수정

--- app/src/main/java/com/starters/hsge/data/api/DogOptionApi.kt
+++ app/src/main/java/com/starters/hsge/data/api/DogOptionApi.kt
@@ -4,6 +4,6 @@ import com.starters.hsge.data.model.remote.response.DogBreedResponse
 import retrofit2.http.GET
 
 interface DogOptionApi {
-    @GET("")
+    @GET("api/common/breed")
     suspend fun getBreed(): DogBreedResponse
 }
