3acc8afb60363235cdd18c0ac50d4772c495e024
seoyoon513 <syoonlee513@gmail.com>
2022-12-05 15:07:33 +0900

    [FEAT] #35 postSignUp endpoint 추가

--- app/src/main/java/com/starters/hsge/data/api/UserApi.kt
+++ app/src/main/java/com/starters/hsge/data/api/UserApi.kt
@@ -5,12 +5,13 @@ import okhttp3.RequestBody
 import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
+import retrofit2.http.PartMap
 
 interface UserApi {
     @Multipart
-    @POST("")
+    @POST("api/auth/signup")
     suspend fun postSignUp(
         @Part dogPhoto: MultipartBody.Part,
-        @Part registerData: RequestBody
+        @PartMap registerData: HashMap<String, RequestBody>
     )
 }
