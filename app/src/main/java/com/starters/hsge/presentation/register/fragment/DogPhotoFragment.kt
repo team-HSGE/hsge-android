5692249e80e05f442de5210f9b7875b2f7e74799
seoyoon513 <syoonlee513@gmail.com>
2022-12-06 15:26:42 +0900

    [REFACTOR] #35 저장소 권한 리팩터링

--- app/src/main/java/com/starters/hsge/presentation/register/fragment/DogPhotoFragment.kt
+++ app/src/main/java/com/starters/hsge/presentation/register/fragment/DogPhotoFragment.kt
@@ -3,109 +3,140 @@ package com.starters.hsge.presentation.register.fragment
 import android.Manifest.permission.READ_EXTERNAL_STORAGE
 import android.app.Activity.RESULT_OK
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS
 import android.view.View
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
 import com.starters.hsge.R
 import com.starters.hsge.databinding.FragmentDogPhotoBinding
 import com.starters.hsge.domain.UriUtil
-import com.starters.hsge.domain.model.RegisterInfo
 import com.starters.hsge.presentation.common.base.BaseFragment
 import com.starters.hsge.presentation.register.viewmodel.RegisterViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class DogPhotoFragment : BaseFragment<FragmentDogPhotoBinding>(R.layout.fragment_dog_photo) {
 
     private lateinit var requestPermissionLauncher: ActivityResultLauncher<String>
-    private val OPEN_GALLERY = 1
+    private lateinit var imageResult: ActivityResultLauncher<Intent>
     private val registerViewModel: RegisterViewModel by viewModels()
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        //https://stackoverflow.com/questions/41928803/how-to-parse-json-in-kotlin
-        val imageResult =
-            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-                if (result.resultCode == RESULT_OK) {
-                    //이미지 받으면 ImageView에 적용
-                    val imageUri = result.data?.data
-                    registerViewModel.dogPhoto = imageUri.toString()
-                    context?.let {
-                        if (imageUri != null) {
 
-                            val registerInfo = RegisterInfo(
-                                email = "slee513@naver.com",
-                                userNickName = "서유니",
-                                userIcon = 221029,
-                                dogAge = "ONE_MONTH",
-                                dogName = "야미",
-                                dogBreed = "SAMOYED",
-                                dogSex = "여자",
-                                dogNeuter = true,
-                                dogLikeTag = "#강아지#고양이#사람",
-                                dogDislikeTag = "#호랑이#오소리#너구리",
-                                latitude = 321.3,
-                                longitude = 222.1
-                            )
+        binding.btnNext.isEnabled = true
 
-                            val uriToFile = UriUtil.toFile(it, imageUri)
-
-                            lifecycleScope.launch {
-                                registerViewModel.postRegisterInfo(uriToFile, registerInfo)
-                            }
-                            setButtonEnable()
-                        }
-                    }
-                    imageUri?.let {
-                        //서버 업로드를 위해 파일 형태로 변환
-                        //imageFile = File(getRealPathFromURI(it))
-
-                        //이미지 불러오기
-                        Glide.with(this)
-                            .load(imageUri)
-                            .fitCenter()
-                            .circleCrop()
-                            .into(binding.ivDogPhoto)
-                    }
-                }
-            }
-
-        // 권한 요청 -> launch로 권한 대화상자 열기
-        requestPermissionLauncher =
-            registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
-                if (isGranted) {
-                    // Permission is granted
-                    // 갤러리 열기
-                    val intent = Intent(Intent.ACTION_GET_CONTENT)
-                    intent.setType("image/*")
-                    startActivityForResult(intent, OPEN_GALLERY)
-
-                    // 이미지 등록
-                    imageResult.launch(intent)
-
-                } else {
-                    // Explain to the user
-                }
-
-            }
-
+        initPermissionLauncher()
+        initImageLauncher()
         initListener()
         setNavigation()
 
     }
 
+    private fun initPermissionLauncher() {
+        requestPermissionLauncher =
+            registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+                if (isGranted) {
+                    navigateGallery()
+                } else {
+                    checkPermission()
+                }
+            }
+    }
+
+    private fun initImageLauncher() {
+        imageResult =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == RESULT_OK) {
+                    val imageUri = result.data?.data
+
+                    context?.let {
+                        if (imageUri != null) {
+                            val imgFile = UriUtil.toFile(it, imageUri)
+                            //registerViewModel.dogPhoto = imgFile
+                            Glide.with(this)
+                                .load(imageUri)
+                                .fitCenter()
+                                .circleCrop()
+                                .into(binding.ivDogPhoto)
+
+                            //setButtonEnable()
+                        }
+                    }
+                }
+            }
+    }
+
+    private fun checkPermission() {
+        val isFirstCheck = prefs.getBoolean("isFistPermissionCheck", true)
+        when {
+
+            ContextCompat.checkSelfPermission(
+                requireContext(),
+                READ_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                navigateGallery()
+            }
+
+            shouldShowRequestPermissionRationale(READ_EXTERNAL_STORAGE) -> {
+                showFirstPermissionDialog()
+            }
+
+            else -> {
+                if (isFirstCheck) {
+                    prefs.edit().putBoolean("isFistPermissionCheck", false).apply()
+                    requestPermissionLauncher.launch(READ_EXTERNAL_STORAGE)
+                } else {
+                    showSecondPermissionDialog()
+                }
+            }
+        }
+    }
+
+    private fun navigateGallery() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "image/*"
+        imageResult.launch(intent)
+    }
+
+    private fun showFirstPermissionDialog() {
+        AlertDialog.Builder(requireContext())
+            .setMessage("앱 실행을 위해서는 권한을 설정해야 합니다")
+            .setPositiveButton("확인") { _, _ ->
+                requestPermissionLauncher.launch(READ_EXTERNAL_STORAGE)
+            }
+            .show()
+    }
+
+    private fun showSecondPermissionDialog() {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setMessage("설정에서 권한을 허용해주세요")
+        builder.setPositiveButton("설정으로 이동하기") { _, _ ->
+            val intent = Intent(
+                ACTION_APPLICATION_DETAILS_SETTINGS, Uri.fromParts(
+                    "package", requireContext().packageName, null
+                )
+            )
+            startActivity(intent)
+        }
+        builder.show()
+    }
+
+
     private fun initListener() {
 
         binding.ivDogPhoto.setOnClickListener {
-            // 시스템 권한 대화상자 요청
-            requestPermissionLauncher.launch(READ_EXTERNAL_STORAGE)
+            checkPermission()
         }
 
         binding.btnNext.setOnClickListener {
@@ -115,7 +146,7 @@ class DogPhotoFragment : BaseFragment<FragmentDogPhotoBinding>(R.layout.fragment
     }
 
     private fun setButtonEnable() {
-        binding.btnNext.isEnabled = !registerViewModel.dogPhoto.isNullOrEmpty()
+        binding.btnNext.isEnabled = !registerViewModel.dogPhoto.exists()
     }
 
     private fun setNavigation() {
