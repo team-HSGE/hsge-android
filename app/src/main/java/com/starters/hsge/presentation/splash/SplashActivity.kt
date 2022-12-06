e7e0b98f195ff278796c9fc277dc69c39e754d72
syb8200 <syb8200@naver.com>
2022-12-06 10:02:52 +0900

    [FEAT] #37 네트워크 상태에 따른 dialog 처리

--- app/src/main/java/com/starters/hsge/presentation/splash/SplashActivity.kt
+++ app/src/main/java/com/starters/hsge/presentation/splash/SplashActivity.kt
@@ -3,26 +3,22 @@ package com.starters.hsge.presentation.splash
 import android.content.Context
 import android.content.Intent
 import android.net.ConnectivityManager
-import android.net.NetworkInfo
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
 import android.util.Log
 import android.view.View
 import android.view.ViewTreeObserver
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.starters.hsge.R
-import com.starters.hsge.databinding.ActivityLoginBinding
+import com.starters.hsge.databinding.ActivitySplashBinding
 import com.starters.hsge.presentation.common.base.BaseActivity
-import com.starters.hsge.presentation.common.base.BaseActivity.Companion.prefs
+import com.starters.hsge.presentation.dialog.SplashDialogFragment
 import com.starters.hsge.presentation.login.LoginActivity
 import com.starters.hsge.presentation.main.MainActivity
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 
-class SplashActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_splash) {
+class SplashActivity : BaseActivity<ActivitySplashBinding>(R.layout.activity_splash) {
 
     var isReady = false
     var accessToken : String = ""
@@ -35,6 +31,7 @@ class SplashActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_spla
         initSplashScreen()
     }
 
+    // 스플래시 초기화
     private fun initSplashScreen() {
         initData()
         getSharedPreference()
@@ -59,22 +56,14 @@ class SplashActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_spla
                     }
                 }
             )
-            //startMainActivity()
-            checkToken()
+            checkNetwork()
         } else {
             // 31 버전이 아닌 경우, 별도 처리.
-            setContentView(R.layout.activity_splash)
-            checkToken()
-            //startMainActivity()
+            checkNetwork()
         }
     }
 
-    private fun getSharedPreference() {
-        accessToken = prefs.getString("accessToken", "")!!
-        refreshToken = prefs.getString("refreshToken", "")!!
-        Log.d("sp", "access: ${accessToken}\nrefresh: ${refreshToken}")
-    }
-
+    // 데이터 처리
     private fun initData() {
         // 별도의 데이터 처리가 없기 때문에 3초의 딜레이를 줌.
         // 선행되어야 하는 작업이 있는 경우, 이곳에서 처리 후 isReady를 변경.
@@ -84,14 +73,62 @@ class SplashActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_spla
         }
     }
 
+    // sp 불러오기
+    private fun getSharedPreference() {
+        accessToken = prefs.getString("accessToken", "")!!
+        refreshToken = prefs.getString("refreshToken", "")!!
+        Log.d("sp", "access: ${accessToken}\nrefresh: ${refreshToken}")
+    }
+
+    // Token 값 확인
     private fun checkToken() {
         if (!prefs.contains("accessToken")){
+            isReady = true
             startLoginActivity()
         } else {
+            isReady = true
             startMainActivity()
         }
     }
 
+    // 네트워크 dialog
+    private fun setNetworkDialog() {
+        val dialog = SplashDialogFragment()
+        dialog.setButtonClickListener(object: SplashDialogFragment.OnButtonClickListener {
+            override fun onOkBtnClicked() {
+                checkNetwork()
+            }
+        })
+        dialog.show(supportFragmentManager, "CustomDialog")
+    }
+
+    // 네트워크 상태 체크 (T/F)
+    private fun isConnected(): Boolean {
+        var connected = false
+        try {
+            val cm = applicationContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            val nInfo = cm.activeNetworkInfo
+            connected = nInfo != null && nInfo.isAvailable && nInfo.isConnected
+            return connected
+
+        } catch (e: Exception) {
+            Log.e("Connectivity Exception", e.message!!)
+        }
+        return connected
+    }
+
+    // 네트워크 상태 체크에 따른 처리
+    private fun checkNetwork() {
+        if (isConnected()) {
+            Log.d("네트워크", "성공!")
+                checkToken()
+        } else {
+            Log.d("네트워크", "실패!")
+                setNetworkDialog()
+        }
+    }
+
+    // 로그인 화면 이동
     private fun startLoginActivity() {
         CoroutineScope(Dispatchers.IO).launch {
             delay(DURATION)
@@ -100,6 +137,7 @@ class SplashActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_spla
         }
     }
 
+    // 메인 화면 이동
     private fun startMainActivity() {
         CoroutineScope(Dispatchers.IO).launch {
             delay(DURATION)
