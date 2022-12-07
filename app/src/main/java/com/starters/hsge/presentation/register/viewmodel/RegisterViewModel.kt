978ffcae6be88a17856eb532243efea2bb99ff01
seoyoon513 <syoonlee513@gmail.com>
2022-12-07 17:54:15 +0900

    [FEAT] #35 liveData 타입 변경 & dataStore save/fetch 함수 추가

--- app/src/main/java/com/starters/hsge/presentation/register/viewmodel/RegisterViewModel.kt
+++ app/src/main/java/com/starters/hsge/presentation/register/viewmodel/RegisterViewModel.kt
@@ -3,9 +3,8 @@ package com.starters.hsge.presentation.register.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
-import com.starters.hsge.data.model.remote.response.Age
-import com.starters.hsge.data.model.remote.response.Breed
 import com.starters.hsge.domain.model.RegisterInfo
+import com.starters.hsge.domain.repository.RegisterPreferencesRepository
 import com.starters.hsge.domain.usecase.GetDogAgeUseCase
 import com.starters.hsge.domain.usecase.GetDogBreedUseCase
 import com.starters.hsge.domain.usecase.PostRegisterUseCase
@@ -20,22 +19,18 @@ class RegisterViewModel @Inject constructor(
     private val postRegisterUseCase: PostRegisterUseCase,
     private val getDogBreedUseCase: GetDogBreedUseCase,
     private val getDogAgeUseCase: GetDogAgeUseCase,
+    private val registerPreferencesRepository: RegisterPreferencesRepository
 ) : BaseViewModel() {
 
-    var dogName = ""
-    var dogPhoto = ""
-    var dogSex = ""
-    var dogNeuter = false
+    lateinit var dogPhoto: File
     var dogAge = ""
     var dogBreed = ""
-    var dogLikTag = ""
-    var dogDisLikeTag = ""
 
-    private val _breedList = MutableLiveData<List<Breed>>()
-    val breedList: LiveData<List<Breed>> = _breedList
+    private val _breedMap = MutableLiveData<Map<String, String>?>()
+    val breedMap: LiveData<Map<String, String>?> = _breedMap
 
-    private val _ageList = MutableLiveData<List<Age>>()
-    val ageList: LiveData<List<Age>> = _ageList
+    private val _ageMap = MutableLiveData<Map<String,String>?>()
+    val ageMap: LiveData<Map<String,String>?> = _ageMap
 
     init {
         getDogBreed()
@@ -46,13 +41,89 @@ class RegisterViewModel @Inject constructor(
 
     private fun getDogBreed() {
         viewModelScope.launch {
-            _breedList.value = getDogBreedUseCase().data
+            _breedMap.value = getDogBreedUseCase()
         }
     }
 
     private fun getDogAge() {
         viewModelScope.launch {
-            _ageList.value = getDogAgeUseCase().data
+            _ageMap.value = getDogAgeUseCase()
         }
+    }
+
+    suspend fun deleteAllInfo() {
+        registerPreferencesRepository.deleteAllData()
+    }
+
+    suspend fun saveDogName(name: String) {
+        registerPreferencesRepository.setDogName(name)
+    }
+
+    suspend fun saveDogSex(sex: String) {
+        registerPreferencesRepository.setDogSex(sex)
+    }
+
+    suspend fun saveDogNeuter(neuter: Boolean) {
+        registerPreferencesRepository.setIsNeuter(neuter)
+    }
+
+    suspend fun saveDogAge(age: String) {
+        registerPreferencesRepository.setDogAge(age)
+    }
+
+    suspend fun saveDogBreed(breed: String) {
+        registerPreferencesRepository.setDogBreed(breed)
+    }
+
+    suspend fun saveDogLikeTag(likeTag: String) {
+        registerPreferencesRepository.setDogLikeTag(likeTag)
+    }
+
+    suspend fun saveDogDislikeTag(disLikeTag: String) {
+        registerPreferencesRepository.setDogDislikeTag(disLikeTag)
+    }
+
+    suspend fun saveUserLatitude(latitude: Double) {
+        registerPreferencesRepository.setUserLatitude(latitude)
+    }
+
+    suspend fun saveUserLongitude(longitude: Double) {
+        registerPreferencesRepository.setUserLongitude(longitude)
+    }
+
+    suspend fun fetchDogName(): String {
+        return registerPreferencesRepository.getDogName()
+    }
+
+    suspend fun fetchDogSex(): String {
+        return registerPreferencesRepository.getDogSex()
+    }
+
+    suspend fun fetchDogNeuter(): Boolean {
+        return registerPreferencesRepository.getIsNeuter()
+    }
+
+    suspend fun fetchDogAge(): String {
+        return registerPreferencesRepository.getDogAge()
+    }
+
+    suspend fun fetchDogBreed(): String {
+        return registerPreferencesRepository.getDogBreed()
+    }
+
+    suspend fun fetchDogLikeTag(): String {
+        return registerPreferencesRepository.getDogLikeTag()
+    }
+
+    suspend fun fetchDogDisLikeTag(): String {
+        return registerPreferencesRepository.getDogDislikeTag()
+    }
+
+    suspend fun fetchUserLatitude(): Double {
+        return registerPreferencesRepository.getUserLatitude()
+    }
+
+    suspend fun fetchUserLongitude(): Double {
+        return registerPreferencesRepository.getUserLongitude()
     }
 }
