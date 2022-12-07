32cfc32771392e58fcf5d47185311fa1f3111fb1
seoyoon513 <syoonlee513@gmail.com>
2022-12-07 17:52:30 +0900

    [FEAT] #35 usecase에서 mapping

--- app/src/main/java/com/starters/hsge/domain/usecase/GetDogAgeUseCase.kt
+++ app/src/main/java/com/starters/hsge/domain/usecase/GetDogAgeUseCase.kt
@@ -1,10 +1,11 @@
 package com.starters.hsge.domain.usecase
 
+import com.starters.hsge.domain.Mapper.mapToAgeMap
 import com.starters.hsge.domain.repository.DogOptionRepository
 import javax.inject.Inject
 
 class GetDogAgeUseCase @Inject constructor(
     private val dogOptionRepository: DogOptionRepository
 ) {
-    suspend operator fun invoke() = dogOptionRepository.getDogAge()
+    suspend operator fun invoke() = dogOptionRepository.getDogAge().mapToAgeMap()
 }
--- app/src/main/java/com/starters/hsge/domain/usecase/GetDogBreedUseCase.kt
+++ app/src/main/java/com/starters/hsge/domain/usecase/GetDogBreedUseCase.kt
@@ -1,10 +1,11 @@
 package com.starters.hsge.domain.usecase
 
+import com.starters.hsge.domain.Mapper.mapToBreedMap
 import com.starters.hsge.domain.repository.DogOptionRepository
 import javax.inject.Inject
 
 class GetDogBreedUseCase @Inject constructor(
     private val dogOptionRepository: DogOptionRepository
 ) {
-    suspend operator fun invoke() = dogOptionRepository.getDogBreed()
+    suspend operator fun invoke() = dogOptionRepository.getDogBreed().mapToBreedMap()
 }
