e69b8cd690edb0952f8d992e5cb99995bbfe6064
안정은 <jjeee1313@gmail.com>
2022-12-08 18:10:25 +0900

    [UI] #42 채팅 리스트 데이터 클래스 구성

---
+++ app/src/main/java/com/starters/hsge/presentation/main/chat/adapter/ChatListAdapter.kt
@@ -0,0 +1,26 @@
+//package com.starters.hsge.presentation.main.chat.adapter
+//
+//import android.view.LayoutInflater
+//import android.view.ViewGroup
+//import androidx.recyclerview.widget.RecyclerView
+//import com.starters.hsge.data.model.ChatList
+//import com.starters.hsge.databinding.ItemChatListBinding
+//import kotlinx.coroutines.NonDisposableHandle.parent
+//
+//class ChatListAdapter(private var chatList: List<ChatList>) : RecyclerView.Adapter<ChatListAdapter.ChatListViewHolder>() {
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChatListViewHolder {
+//        val
+//    }
+//
+//    override fun onBindViewHolder(holder: ChatListViewHolder, position: Int) {
+//        TODO("Not yet implemented")
+//    }
+//
+//    override fun getItemCount(): Int {
+//        TODO("Not yet implemented")
+//    }
+//
+//    class ChatListViewHolder {
+//
+//    }
+//}
