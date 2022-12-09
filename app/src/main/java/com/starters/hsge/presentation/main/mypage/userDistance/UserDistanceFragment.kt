6614eda3a777dce9d3084f9b028d5431d251abe1
안정은 <jjeee1313@gmail.com>
2022-12-09 12:14:52 +0900

    [FIX] #39 내 위치 설정 이전에 설정한 값 post되는 버그 수정

--- app/src/main/java/com/starters/hsge/presentation/main/mypage/userDistance/UserDistanceFragment.kt
+++ app/src/main/java/com/starters/hsge/presentation/main/mypage/userDistance/UserDistanceFragment.kt
@@ -16,31 +16,34 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class UserDistanceFragment :
-    BaseFragment<FragmentUserDistanceBinding>(R.layout.fragment_user_distance) {
+class UserDistanceFragment : BaseFragment<FragmentUserDistanceBinding>(R.layout.fragment_user_distance) {
 
-    val distance = prefs.getInt("distance", 3)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        initListener()
         seekbar()
+        initListener()
         setNavigation()
 
     }
 
+    // 완료 버튼
     private fun initListener() {
         binding.btnNext.setOnClickListener {
             // post로 api 통신하면 됨
             findNavController().navigate(R.id.action_userDistanceFragment_to_homeFragment)
             (activity as MainActivity).binding.navigationMain.visibility = View.VISIBLE
 
+            val distance = prefs.getInt("distance", 3)
             retrofitWork(distance.toDouble())
         }
     }
 
+
+    // seekBar - 반경 설정
     private fun seekbar() {
         // 이전에 정했던 반경으로 디폴트 값
+        val distance = prefs.getInt("distance", 3)
         binding.userDistanceSeekbar.setProgress(distance-3)
         binding.tvLocationDistance.text = "${distance}km"
 
@@ -62,17 +65,18 @@ class UserDistanceFragment :
             override fun onStopTrackingTouch(p0: SeekBar?) {
             }
         })
-
-
-
     }
 
+
+    // 화면 이동
     private fun setNavigation() {
         binding.toolBar.setNavigationOnClickListener {
             findNavController().navigateUp()
         }
     }
 
+
+    // 반경 재설정_통신
     private fun retrofitWork(distance: Double){
         val distanceRetrofit = RetrofitApi.retrofit.create(DistanceService::class.java)
         val radius = distance / 100
@@ -80,6 +84,7 @@ class UserDistanceFragment :
         distanceRetrofit.putDistanceData(request = Distance(radius)).enqueue(object : Callback<Void>{
             override fun onResponse(call: Call<Void>, response: Response<Void>) {
                 if(response.isSuccessful){
+                    Log.d("distance", radius.toString())
                     Log.d("distance", response.toString())
                     Log.d("distance", "성공")
                 } else{
