a4f25349028a604dd81cdbaf15cf21afe048ee89
seoyoon513 <syoonlee513@gmail.com>
2022-12-12 15:43:52 +0900

    [FEAT] #53 유저 강아지 목록 데이터 바인딩

--- app/src/main/java/com/starters/hsge/presentation/main/management/ManagementFragment.kt
+++ app/src/main/java/com/starters/hsge/presentation/main/management/ManagementFragment.kt
@@ -3,58 +3,29 @@ package com.starters.hsge.presentation.main.management
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.starters.hsge.R
 import com.starters.hsge.databinding.FragmentManagementBinding
 import com.starters.hsge.presentation.common.base.BaseFragment
 import com.starters.hsge.presentation.common.util.CustomDecoration
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class ManagementFragment : BaseFragment<FragmentManagementBinding>(R.layout.fragment_management) {
+
+    private val managementViewModel: ManagementViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val list = listOf(
-            Doggi(
-                R.drawable.ic_dog_profile_1,
-                "몽이",
-                R.drawable.ic_gender_male_black,
-                "1개월",
-                "포메라니안"
-            ),
-            Doggi(
-                R.drawable.ic_dog_profile_1,
-                "몽이",
-                R.drawable.ic_gender_male_black,
-                "1개월",
-                "포메라니안"
-            ),
-            Doggi(
-                R.drawable.ic_dog_profile_1,
-                "몽이",
-                R.drawable.ic_gender_male_black,
-                "1개월",
-                "포메라니안"
-            )
-        )
-
-
         val customDecoration = CustomDecoration(1f, 0f, Color.parseColor("#EFEFEF"))
         binding.rvDogList.addItemDecoration(customDecoration)
 
-
-        val adapter = DogListAdapter(list)
-        binding.rvDogList.layoutManager = LinearLayoutManager(context)
-        binding.rvDogList.adapter = adapter
-
+        managementViewModel.myDogList.observe(viewLifecycleOwner) {
+            val adapter = it?.let { it1 -> DogListAdapter(it1) }
+            binding.rvDogList.layoutManager = LinearLayoutManager(context)
+            binding.rvDogList.adapter = adapter
+        }
     }
 }
-
-
-data class Doggi(
-    val dogPhoto: Int,
-    val dogName: String,
-    val dogSex: Int,
-    val dogAge: String,
-    val dogBreed: String,
-)
