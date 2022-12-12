34fa1da7951f51bff437de08c81f472f2a3ae48a
안정은 <jjeee1313@gmail.com>
2022-12-12 19:56:49 +0900

    [CHORE] #39 토큰 확인 코드 추가

--- app/src/main/java/com/starters/hsge/presentation/main/home/HomeFragment.kt
+++ app/src/main/java/com/starters/hsge/presentation/main/home/HomeFragment.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.view.animation.AccelerateInterpolator
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
 import com.starters.hsge.R
 import com.starters.hsge.data.model.remote.response.DogCard
 import com.starters.hsge.data.model.IsLikeRequest
+import com.starters.hsge.data.model.remote.response.Tag
 import com.starters.hsge.databinding.FragmentHomeBinding
 import com.starters.hsge.presentation.common.base.BaseFragment
 import com.starters.hsge.presentation.main.home.adapter.CardStackAdapter
@@ -32,124 +35,85 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
 
         retrofitWork()
 
-//
-//        dogCardList = listOf<DogCard>(
-//            DogCard(
-//                "뽀삐",
-//                1,
-//                "https://user-images.githubusercontent.com/106398273/204183502-2974fadc-dae6-4238-83ce-3b4acd12373d.jpeg",
-//                "스탠다드 푸들",
-//                " I 12살 10개월 I ",
-//                "Female",
-//                true,
-//                Tag(listOf("고구마랄라", "간식감자", "수영좋아"), listOf("산책", "카메라"))
-//            ),
-//            DogCard(
-//                "뽀삐",
-//                2,
-//                "https://i.guim.co.uk/img/media/fe1e34da640c5c56ed16f76ce6f994fa9343d09d/0_174_3408_2046/master/3408.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=0d3f33fb6aa6e0154b7713a00454c83d",
-//                "스탠다드 푸들",
-//                " I 12살 10개월 I ",
-//                "Female",
-//                true,
-//                Tag(listOf("고구마", "간식", "수영"), listOf("산책", "카메라"))
-//            ),
-//            DogCard(
-//                "뽀삐",
-//                3,
-//                "https://user-images.githubusercontent.com/106398273/204183502-2974fadc-dae6-4238-83ce-3b4acd12373d.jpeg",
-//                "스탠다드 푸들",
-//                " I 12살 10개월 I ",
-//                "Female",
-//                true,
-//                Tag(listOf("고구마랄라", "간식감자", "수영좋아"), listOf("산책", "카메라"))
-//            ),
-//            DogCard(
-//                "뽀삐",
-//                4,
-//                "https://i.guim.co.uk/img/media/fe1e34da640c5c56ed16f76ce6f994fa9343d09d/0_174_3408_2046/master/3408.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=0d3f33fb6aa6e0154b7713a00454c83d",
-//                "스탠다드 푸들",
-//                " I 12살 10개월 I ",
-//                "Female",
-//                true,
-//                Tag(listOf("고구마", "간식", "수영"), listOf("산책", "카메라"))
-//            ),
-//            DogCard(
-//                "뽀삐",
-//                5,
-//                "https://user-images.githubusercontent.com/106398273/204183502-2974fadc-dae6-4238-83ce-3b4acd12373d.jpeg",
-//                "스탠다드 푸들",
-//                " I 12살 10개월 I ",
-//                "Female",
-//                true,
-//                Tag(listOf("고구마랄라", "간식감자", "수영좋아"), listOf("산책", "카메라"))
-//            ),
-//            DogCard(
-//                "뽀삐",
-//                6,
-//                "https://i.guim.co.uk/img/media/fe1e34da640c5c56ed16f76ce6f994fa9343d09d/0_174_3408_2046/master/3408.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=0d3f33fb6aa6e0154b7713a00454c83d",
-//                "스탠다드 푸들",
-//                " I 12살 10개월 I ",
-//                "Female",
-//                true,
-//                Tag(listOf("고구마", "간식", "수영"), listOf("산책", "카메라"))
-//            ),
-//            DogCard(
-//                "뽀삐",
-//                7,
-//                "https://user-images.githubusercontent.com/106398273/204183502-2974fadc-dae6-4238-83ce-3b4acd12373d.jpeg",
-//                "스탠다드 푸들",
-//                " I 12살 10개월 I ",
-//                "Female",
-//                true,
-//                Tag(listOf("고구마랄라", "간식감자", "수영좋아"), listOf("산책", "카메라"))
-//            ),
-//            DogCard(
-//                "뽀삐",
-//                8,
-//                "https://i.guim.co.uk/img/media/fe1e34da640c5c56ed16f76ce6f994fa9343d09d/0_174_3408_2046/master/3408.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=0d3f33fb6aa6e0154b7713a00454c83d",
-//                "스탠다드 푸들",
-//                " I 12살 10개월 I ",
-//                "Female",
-//                true,
-//                Tag(listOf("고구마", "간식", "수영"), listOf("산책", "카메라"))
-//            )
-//        )
-//
-//        manager = CardStackLayoutManager(context, object : CardStackListener {
-//            override fun onCardDragging(direction: Direction?, ratio: Float) {
-//            }
-//
-//            override fun onCardSwiped(direction: Direction?) {
-//                when (direction) {
-//                    Direction.Right -> swipeIsLike(dogCardList, isLike = true)
-//                    Direction.Left -> swipeIsLike(dogCardList, isLike = false)
-//                    else -> {
-//
-//                    }
-//                }
-//            }
-//
-//            override fun onCardRewound() {
-//            }
-//
-//            override fun onCardCanceled() {
-//            }
-//
-//            override fun onCardAppeared(view: View?, position: Int) {
-//            }
-//
-//            override fun onCardDisappeared(view: View?, position: Int) {
-//            }
-//
-//        })
-//
-//
-//        // 로컬 데이터
-//        cardStackAdapter = CardStackAdapter(requireContext(), dogCardList)
-//        manager.setSwipeableMethod(SwipeableMethod.AutomaticAndManual)
-//        binding.cardStackView.layoutManager = manager
-//        binding.cardStackView.adapter = cardStackAdapter
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w("firebaseToken", "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+
+            // Get new FCM registration token
+            val token = task.result
+
+            Log.d("firebaseToken", token.toString())
+        })
+
+        dogCardList = listOf<DogCard>(
+            DogCard(
+                1,
+                "뽀삐",
+                "https://user-images.githubusercontent.com/106398273/204183502-2974fadc-dae6-4238-83ce-3b4acd12373d.jpeg",
+                "스탠다드 푸들",
+                " I 12살 10개월 I ",
+                "Female",
+                true,
+                Tag(listOf("고구마랄라", "간식감자", "수영좋아"), listOf("산책", "카메라"))
+            ),
+            DogCard(
+                2,
+                "뽀삐",
+                "https://i.guim.co.uk/img/media/fe1e34da640c5c56ed16f76ce6f994fa9343d09d/0_174_3408_2046/master/3408.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=0d3f33fb6aa6e0154b7713a00454c83d",
+                "스탠다드 푸들",
+                " I 12살 10개월 I ",
+                "Female",
+                true,
+                Tag(listOf("고구마", "간식", "수영"), listOf("산책", "카메라"))
+            ),
+            DogCard(
+                3,
+                "뽀삐",
+                "https://user-images.githubusercontent.com/106398273/204183502-2974fadc-dae6-4238-83ce-3b4acd12373d.jpeg",
+                "스탠다드 푸들",
+                " I 12살 10개월 I ",
+                "Female",
+                true,
+                Tag(listOf("고구마랄라", "간식감자", "수영좋아"), listOf("산책", "카메라"))
+            )
+        )
+
+        manager = CardStackLayoutManager(context, object : CardStackListener {
+            override fun onCardDragging(direction: Direction?, ratio: Float) {
+            }
+
+            override fun onCardSwiped(direction: Direction?) {
+                when (direction) {
+                    Direction.Right -> swipeIsLike(dogCardList, isLike = true)
+                    Direction.Left -> swipeIsLike(dogCardList, isLike = false)
+                    else -> {
+
+                    }
+                }
+            }
+
+            override fun onCardRewound() {
+            }
+
+            override fun onCardCanceled() {
+            }
+
+            override fun onCardAppeared(view: View?, position: Int) {
+            }
+
+            override fun onCardDisappeared(view: View?, position: Int) {
+            }
+
+        })
+
+
+        // 로컬 데이터
+        cardStackAdapter = CardStackAdapter(requireContext(), dogCardList)
+        manager.setSwipeableMethod(SwipeableMethod.AutomaticAndManual)
+        binding.cardStackView.layoutManager = manager
+        binding.cardStackView.adapter = cardStackAdapter
 
 
         fabClick(binding.cardStackView)
@@ -196,33 +160,33 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
                     val dogCardResult = response.body()
                     cardStackAdapter = CardStackAdapter(requireContext(), dogCardResult!!)
 
-                    manager = CardStackLayoutManager(context, object : CardStackListener {
-                        override fun onCardDragging(direction: Direction?, ratio: Float) {
-                        }
-
-                        override fun onCardSwiped(direction: Direction?) {
-                            when (direction) {
-                                Direction.Right -> swipeIsLike(dogCardResult, isLike = true)
-                                Direction.Left -> swipeIsLike(dogCardResult, isLike = false)
-                                else -> {
-
-                                }
-                            }
-                        }
-
-                        override fun onCardRewound() {
-                        }
-
-                        override fun onCardCanceled() {
-                        }
-
-                        override fun onCardAppeared(view: View?, position: Int) {
-                        }
-
-                        override fun onCardDisappeared(view: View?, position: Int) {
-                        }
-
-                    })
+//                    manager = CardStackLayoutManager(context, object : CardStackListener {
+//                        override fun onCardDragging(direction: Direction?, ratio: Float) {
+//                        }
+//
+//                        override fun onCardSwiped(direction: Direction?) {
+//                            when (direction) {
+//                                Direction.Right -> swipeIsLike(dogCardResult, isLike = true)
+//                                Direction.Left -> swipeIsLike(dogCardResult, isLike = false)
+//                                else -> {
+//
+//                                }
+//                            }
+//                        }
+//
+//                        override fun onCardRewound() {
+//                        }
+//
+//                        override fun onCardCanceled() {
+//                        }
+//
+//                        override fun onCardAppeared(view: View?, position: Int) {
+//                        }
+//
+//                        override fun onCardDisappeared(view: View?, position: Int) {
+//                        }
+//
+//                    })
 
                     manager.setSwipeableMethod(SwipeableMethod.AutomaticAndManual)
                     binding.cardStackView.layoutManager = manager
