6f8ba06c2783c897492adaec4c6f64dbc9f95c4f
syb8200 <syb8200@naver.com>
2022-12-12 19:10:30 +0900

    [FEAT] #56 사용자 프로필 이미지 복구

--- app/src/main/java/com/starters/hsge/presentation/register/adapter/UserProfileIconAdapter.kt
+++ app/src/main/java/com/starters/hsge/presentation/register/adapter/UserProfileIconAdapter.kt
@@ -1,30 +1,25 @@
 package com.starters.hsge.presentation.register.adapter
 
-import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.starters.hsge.R
 import com.starters.hsge.databinding.ItemProfileIconBinding
-import com.starters.hsge.presentation.common.base.BaseFragment
 import com.starters.hsge.presentation.common.base.BaseFragment.Companion.prefs
 
 class UserProfileIconAdapter(private var userIconList: List<Int>)
     : RecyclerView.Adapter<UserProfileIconAdapter.UserProfileIconViewHolder>() {
-
-    private lateinit var itemClickListener : OnItemClickListener
 
     inner class UserProfileIconViewHolder(private val binding: ItemProfileIconBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(resId: Int) {
             binding.icon = resId
             binding.executePendingBindings()
-
             itemView.setOnClickListener {
-                itemClickListener.onClick(it, resId)
-                Log.d("이미지ID", "${resId}")
+                prefs.edit().putInt("resId", resId).apply()
+                Navigation.findNavController(binding.root)
+                    .navigate(R.id.action_userProfileIconFragment_to_userImageFragment)
             }
 
         }
@@ -44,14 +39,6 @@ class UserProfileIconAdapter(private var userIconList: List<Int>)
 
     override fun getItemCount(): Int {
         return userIconList.size
-    }
-
-    interface OnItemClickListener{
-        fun onClick(v: View, resId: Int)
-    }
-
-    fun setItemClickListener(onItemClickListener: OnItemClickListener) {
-        this.itemClickListener = onItemClickListener
     }
 
 }
--- app/src/main/java/com/starters/hsge/presentation/register/fragment/UserImageFragment.kt
+++ app/src/main/java/com/starters/hsge/presentation/register/fragment/UserImageFragment.kt
@@ -2,97 +2,50 @@ package com.starters.hsge.presentation.register.fragment
 
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.viewModels
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
 import com.starters.hsge.R
 import com.starters.hsge.databinding.FragmentUserImageBinding
 import com.starters.hsge.presentation.common.base.BaseFragment
-import com.starters.hsge.presentation.register.viewmodel.RegisterViewModel
-import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.launch
 
-@AndroidEntryPoint
 class UserImageFragment : BaseFragment<FragmentUserImageBinding>(R.layout.fragment_user_image) {
 
-    var resId : Int = 0
-    private val registerViewModel: RegisterViewModel by viewModels()
+    var resId : Int? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        lifecycleScope.launch {
-            resId = registerViewModel.fetchUserIcon().first()
-            Log.d("이미지ID3", "${resId}")
-        }
-
-        if (resId != 0){
+        initListener()
+        setNavigation()
+        getSharedPreferences()
+        if (prefs.contains("resId")){
             binding.btnNext.isEnabled = true
-            binding.ivUserImage.setImageResource(resId)
-
-            binding.ivUserImage.setOnClickListener {
-                Navigation.findNavController(binding.root)
-                    .navigate(R.id.action_userImageFragment_to_userProfileIconFragment)
-            }
-
+            binding.ivUserImage.setImageResource(resId!!)
         } else {
-            binding.ivUserImage.setOnClickListener {
-                Navigation.findNavController(binding.root)
-                    .navigate(R.id.action_userImageFragment_to_userProfileIconFragment)
-            }
+            return
         }
+    }
 
+    private fun initListener() {
         binding.btnNext.setOnClickListener {
-            if(resId != 0) {
+            if(prefs.contains("resId")) {
                 Navigation.findNavController(binding.root)
                     .navigate(R.id.action_userImageFragment_to_dogPhotoFragment)
-
-                lifecycleScope.launch{
-                    registerViewModel.deleteAllInfo()
-                }
-
             } else {
                 return@setOnClickListener
             }
         }
 
-
-
-
-        //initListener()
-        setNavigation()
+        binding.ivUserImage.setOnClickListener {
+            Navigation.findNavController(binding.root)
+                .navigate(R.id.action_userImageFragment_to_userProfileIconFragment)
+        }
     }
 
-//    private fun initListener() {
-//        binding.btnNext.setOnClickListener {
-//            if(resId != 0) {
-//                Navigation.findNavController(binding.root)
-//                    .navigate(R.id.action_userImageFragment_to_dogPhotoFragment)
-//
-//                lifecycleScope.launch{
-//                    registerViewModel.deleteAllInfo()
-//                }
-//
-//            } else {
-//                return@setOnClickListener
-//            }
-//        }
-//
-//        binding.ivUserImage.setOnClickListener {
-//            Navigation.findNavController(binding.root)
-//                .navigate(R.id.action_userImageFragment_to_userProfileIconFragment)
-//        }
-//    }
-
     private fun getSharedPreferences() {
-//        resId = prefs.getInt("resId", 0)
-//        Log.d("sp", "${resId}")
-
+        resId = prefs.getInt("resId", 2131165412)
+        Log.d("sp", "${resId}")
     }
 
     private fun setNavigation() {
