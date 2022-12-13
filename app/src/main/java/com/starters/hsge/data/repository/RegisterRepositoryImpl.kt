e899a7bfba5b817dc6ea15962c1603eb8e395388
seoyoon513 <syoonlee513@gmail.com>
2022-12-13 14:04:35 +0900

    [FEAT] #60 반환값 Flow 추가

--- app/src/main/java/com/starters/hsge/data/repository/RegisterRepositoryImpl.kt
+++ app/src/main/java/com/starters/hsge/data/repository/RegisterRepositoryImpl.kt
@@ -1,10 +1,14 @@
 package com.starters.hsge.data.repository
 
+import com.starters.hsge.ApiResult
 import com.starters.hsge.data.api.UserApi
+import com.starters.hsge.data.model.remote.request.SignUpResponse
 import com.starters.hsge.di.RetrofitHSGE
 import com.starters.hsge.domain.FormDataUtil
 import com.starters.hsge.domain.model.RegisterInfo
 import com.starters.hsge.domain.repository.RegisterRepository
+import com.starters.hsge.safeFlow
+import kotlinx.coroutines.flow.Flow
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import okhttp3.RequestBody
@@ -14,7 +18,8 @@ import javax.inject.Inject
 class RegisterRepositoryImpl @Inject constructor(
     @RetrofitHSGE private val userApi: UserApi
 ) : RegisterRepository {
-    override suspend fun postUserData(img: File, data: RegisterInfo) {
+    override suspend fun postUserData(img: File, data: RegisterInfo): Flow<ApiResult<SignUpResponse>> =
+        safeFlow {
         val formJson = Json.encodeToString(data)
         val formMultipart = FormDataUtil.getImageBody("imgFile", img)
         val formRequestBody = FormDataUtil.getJsonBody(formJson)
