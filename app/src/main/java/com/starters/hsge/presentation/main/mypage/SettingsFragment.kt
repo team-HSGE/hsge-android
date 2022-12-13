b5b8a72574bcd224b992286267224d182a1dc519
syb8200 <syb8200@naver.com>
2022-12-13 21:01:53 +0900

    [FEAT] #61 로그아웃 기능 구현

--- app/src/main/java/com/starters/hsge/presentation/main/mypage/SettingsFragment.kt
+++ app/src/main/java/com/starters/hsge/presentation/main/mypage/SettingsFragment.kt
@@ -1,12 +1,17 @@
 package com.starters.hsge.presentation.main.mypage
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.navigation.fragment.findNavController
 import com.starters.hsge.R
 import com.starters.hsge.databinding.FragmentSettingsBinding
 import com.starters.hsge.presentation.common.base.BaseFragment
 import com.starters.hsge.presentation.dialog.LogoutDialogFragment
+import com.starters.hsge.presentation.login.LoginActivity
 import com.starters.hsge.presentation.main.MainActivity
 
 class SettingsFragment : BaseFragment<FragmentSettingsBinding>(R.layout.fragment_settings) {
@@ -30,6 +35,9 @@ class SettingsFragment : BaseFragment<FragmentSettingsBinding>(R.layout.fragment
 
                 override fun onLogoutBtnClicked() {
                     // 확인 버튼 클릭했을 때 처리
+                    prefs.edit().clear().apply()
+                    moveToLoginActivity()
+                    Toast.makeText(context, "로그아웃 되었습니다.", Toast.LENGTH_SHORT).show()
                 }
             })
 
@@ -44,7 +52,13 @@ class SettingsFragment : BaseFragment<FragmentSettingsBinding>(R.layout.fragment
         }
     }
 
-    fun visibleBtmNav(){
+    private fun visibleBtmNav(){
         (activity as MainActivity).binding.navigationMain.visibility = View.VISIBLE
+    }
+
+    private fun moveToLoginActivity() {
+        val intent = Intent(requireActivity(), LoginActivity::class.java)
+        ActivityCompat.finishAffinity(requireActivity())
+        startActivity(intent)
     }
 }
