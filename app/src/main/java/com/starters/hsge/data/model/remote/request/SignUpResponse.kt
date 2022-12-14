2b9eaa8b8c5e41d4beac09bb34c10a93ea467449
seoyoon513 <syoonlee513@gmail.com>
2022-12-14 11:52:59 +0900

    [FIX] #60 SignUpResponse에 @Serializable 추가

--- app/src/main/java/com/starters/hsge/data/model/remote/request/SignUpResponse.kt
+++ app/src/main/java/com/starters/hsge/data/model/remote/request/SignUpResponse.kt
@@ -1,5 +1,8 @@
 package com.starters.hsge.data.model.remote.request
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class SignUpResponse(
     val accessToken: String,
     val refreshToken: String
