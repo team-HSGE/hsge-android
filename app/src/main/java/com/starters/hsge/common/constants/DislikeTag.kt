1e66a217a72825d893aa8ddcac02f29d93960570
seoyoon513 <syoonlee513@gmail.com>
2022-12-17 15:45:04 +0900

    [FEAT] #81 likeTag, dislikeTag enum class 추가

---
+++ app/src/main/java/com/starters/hsge/common/constants/DislikeTag.kt
@@ -0,0 +1,22 @@
+package com.starters.hsge.common.constants
+
+enum class DislikeTag(val tag: String) {
+    MAN("남자사람"),
+    WOMAN("여자사람"),
+    CHILD("아이"),
+    HUMAN("사람"),
+    MALE("수컷"),
+    FEMALE("암컷"),
+    LARGE_DOG("대형견"),
+    MEDIUM_DOG("중형견"),
+    SMALL_DOG("소형견"),
+    SWIM("수영"),
+    DRESSING("옷입기"),
+    PICTURE("사진찍기"),
+    KISS("뽀뽀"),
+    TOUCH_PAW("발만지기"),
+    TOUCH_TAIL("꼬리만지기"),
+    TOUCH("스킨십"),
+    LOUD_NOISE("큰소리"),
+    PERFUME("향수")
+}
