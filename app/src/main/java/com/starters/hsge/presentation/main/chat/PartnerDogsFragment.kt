600f4cc4ef81493ef0fa9fa7476566012176e0e8
syb8200 <syb8200@naver.com>
2022-12-21 19:27:24 +0900

    [UI] #104 채팅 상대방 반려견 리스트 화면 작성

---
+++ app/src/main/java/com/starters/hsge/presentation/main/chat/PartnerDogsFragment.kt
@@ -0,0 +1,14 @@
+package com.starters.hsge.presentation.main.chat
+
+import android.os.Bundle
+import android.view.View
+import com.starters.hsge.R
+import com.starters.hsge.databinding.FragmentChatPartnerDogsBinding
+import com.starters.hsge.presentation.common.base.BaseFragment
+
+class PartnerDogsFragment: BaseFragment<FragmentChatPartnerDogsBinding>(R.layout.fragment_chat_partner_dogs) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
