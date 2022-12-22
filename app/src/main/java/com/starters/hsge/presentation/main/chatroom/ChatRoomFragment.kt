9a11f2dc21ab41a3a2bd91aa277dc7f139a1e8e0
seoyoon513 <syoonlee513@gmail.com>
2022-12-22 11:12:11 +0900

    [FEAT] #97 okhttp3로 서버와 test websocket 통신

--- app/src/main/java/com/starters/hsge/presentation/main/chatroom/ChatRoomFragment.kt
+++ app/src/main/java/com/starters/hsge/presentation/main/chatroom/ChatRoomFragment.kt
@@ -5,10 +5,25 @@ import android.view.View
 import com.starters.hsge.R
 import com.starters.hsge.databinding.FragmentChatRoomBinding
 import com.starters.hsge.presentation.common.base.BaseFragment
+import okhttp3.OkHttpClient
+import okhttp3.Request
 
 class ChatRoomFragment : BaseFragment<FragmentChatRoomBinding>(R.layout.fragment_chat_room) {
 
+    private lateinit var client: OkHttpClient
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        client = OkHttpClient()
+
+        val request: Request = Request.Builder()
+            .url("ws://192.168.0.59:8082/ws/chat")
+            .build()
+        val listener = WebSocketListener()
+
+        client.newWebSocket(request, listener)
+        client.dispatcher.executorService.shutdown()
+
     }
 }
