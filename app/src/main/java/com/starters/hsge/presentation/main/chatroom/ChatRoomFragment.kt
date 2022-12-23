6f586b9ac472f8f8d3ec85bab6153cca758b601b
seoyoon513 <syoonlee513@gmail.com>
2022-12-23 11:47:05 +0900

    [FEAT] #97 stomp 기본 셋팅 & 네비게이션 처리

--- app/src/main/java/com/starters/hsge/presentation/main/chatroom/ChatRoomFragment.kt
+++ app/src/main/java/com/starters/hsge/presentation/main/chatroom/ChatRoomFragment.kt
@@ -1,29 +1,129 @@
 package com.starters.hsge.presentation.main.chatroom
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import androidx.activity.OnBackPressedCallback
+import androidx.navigation.fragment.findNavController
 import com.starters.hsge.R
 import com.starters.hsge.databinding.FragmentChatRoomBinding
 import com.starters.hsge.presentation.common.base.BaseFragment
+import com.starters.hsge.presentation.main.MainActivity
+import dagger.hilt.android.AndroidEntryPoint
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import okio.Okio
+import org.json.JSONObject
+import ua.naiksoftware.stomp.Stomp
+import ua.naiksoftware.stomp.StompClient
+import ua.naiksoftware.stomp.dto.LifecycleEvent
+import ua.naiksoftware.stomp.dto.StompHeader
 
+@AndroidEntryPoint
 class ChatRoomFragment : BaseFragment<FragmentChatRoomBinding>(R.layout.fragment_chat_room) {
 
-    private lateinit var client: OkHttpClient
+    private lateinit var callback: OnBackPressedCallback
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        client = OkHttpClient()
+        /**
+         * @author
+         * initListener()를 타고 가시면 툴바 버튼 클릭 시 action을 정의할 수 있습니다
+         */
 
-        val request: Request = Request.Builder()
-            .url("ws://192.168.0.59:8082/ws/chat")
-            .build()
-        val listener = WebSocketListener()
+        initListener()
+        setNavigation()
 
-        client.newWebSocket(request, listener)
-        client.dispatcher.executorService.shutdown()
+        val url = "ws://[domain]/connect/websocket"
+        val intervalMillis = 1000L
+        val stompClient = Stomp.over(Stomp.ConnectionProvider.OKHTTP, url)
 
+//        fun runStomp() {
+//            stompClient.topic("/topic/~").subscribe { topicMessage ->
+//                Log.i("message Receive", topicMessage.payload)
+//            }
+//
+//
+//            // 헤더를 넣고 싶으면 connect할 때 설정한 헤더들을 넣어주면 됨
+//            val headerList = arrayListOf<StompHeader>()
+//            headerList.add(StompHeader("inviteCode","test0912"))
+//            headerList.add(StompHeader("username", text.value))
+//            headerList.add(StompHeader("positionType", "1"))
+//            stompClient.connect(headerList)
+//
+//
+//            //stopmClient의 lifecycle 변경에 따라 로그를 찍는 코드
+//            stompClient.lifecycle().subscribe { lifecycleEvent ->
+//                when (lifecycleEvent.type) {
+//                    LifecycleEvent.Type.OPENED -> {
+//                        Log.i("OPEND", "!!")
+//                    }
+//                    LifecycleEvent.Type.CLOSED -> {
+//                        Log.i("CLOSED", "!!")
+//
+//                    }
+//                    LifecycleEvent.Type.ERROR -> {
+//                        Log.i("ERROR", "!!")
+//                        Log.e("CONNECT ERROR", lifecycleEvent.exception.toString())
+//                    }
+//                    else ->{
+//                        Log.i("ELSE", lifecycleEvent.message)
+//                    }
+//                }
+//            }
+//
+//            // 채팅을 보낼 때 채팅 메세지의 값에 맞춰서 보낼 주소에 넣어주기기
+//            val data =JSONObject()
+//            data.put("userKey", text.value)
+//            data.put("positionType", "1")
+//            data.put("content", "test")
+//            data.put("messageType", "CHAT")
+//            data.put("destRoomCode", "test0912")
+//
+//            //정책에 맞게 문자 전송
+//            stompClient.send("/stream/chat/send", data.toString()).subscribe()
+//
+//        }
+    }
+
+    private fun initListener() {
+        binding.toolBar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.menu_report -> {
+                    //TODO : 바텀시트 호출을 여기서 해주세요
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                findNavController().navigateUp()
+                visibleBtmNav()
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(this, callback)
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        callback.remove()
+    }
+
+    private fun setNavigation() {
+        binding.toolBar.setNavigationOnClickListener {
+            findNavController().navigateUp()
+            visibleBtmNav()
+        }
+    }
+
+    private fun visibleBtmNav(){
+        (activity as MainActivity).binding.navigationMain.visibility = View.VISIBLE
     }
 }
