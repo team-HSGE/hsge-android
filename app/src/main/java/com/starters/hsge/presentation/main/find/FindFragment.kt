4ff8a7642524383090194bd2e630fdd990a830dd
syb8200 <syb8200@naver.com>
2022-12-28 03:48:10 +0900

    [FEAT] #113 locationRequest 처리

--- app/src/main/java/com/starters/hsge/presentation/main/find/FindFragment.kt
+++ app/src/main/java/com/starters/hsge/presentation/main/find/FindFragment.kt
@@ -1,12 +1,14 @@
 package com.starters.hsge.presentation.main.find
 
 import android.Manifest
-import android.app.Activity
+import android.annotation.SuppressLint
 import android.app.AlertDialog
 import android.content.Context
 import android.content.Intent
+import android.content.IntentSender
 import android.content.pm.PackageManager
 import android.graphics.Color
+import android.location.Geocoder
 import android.location.Location
 import android.location.LocationManager
 import android.net.Uri
@@ -17,11 +19,14 @@ import android.view.View
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.lifecycleScope
-import com.bumptech.glide.Glide
-import com.starters.hsge.BuildConfig
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
+import com.google.android.gms.tasks.CancellationToken
+import com.google.android.gms.tasks.CancellationTokenSource
+import com.google.android.gms.tasks.OnTokenCanceledListener
+import com.google.android.gms.tasks.Task
 import com.starters.hsge.R
 import com.starters.hsge.databinding.FragmentFindBinding
 import com.starters.hsge.presentation.common.base.BaseFragment
@@ -30,76 +35,92 @@ import net.daum.mf.map.api.*
 
 class FindFragment : BaseFragment<FragmentFindBinding>(R.layout.fragment_find) {
 
-    private lateinit var locationPermissionRequest: ActivityResultLauncher<String>
-
-    private var mCurrentLat : Double? = 0.0
-    private var mCurrentLng : Double? = 0.0
-    private var status : Boolean = true
+    private lateinit var locationPermissionRequest: ActivityResultLauncher<Array<String>>
+    private var fusedLocationClient: FusedLocationProviderClient? = null
+    private var mCurrentLat: Double? = 0.0
+    private var mCurrentLng: Double? = 0.0
+    private var status: Boolean = true
+    private val TAG = "SOL_LOG"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         initPermissionLauncher()
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        checkPermission()
+        startLocationUpdates()
         setZoomLevel()
         setTrackingBtn()
         catchCurrentLocation()
     }
 
+    override fun onResume() {
+        super.onResume()
+        setAutoLocation()
+
+    }
+
     override fun onStop() {
         super.onStop()
-
         binding.kakaoMapView.currentLocationTrackingMode = MapView.CurrentLocationTrackingMode.TrackingModeOff
         binding.kakaoMapView.setShowCurrentLocationMarker(false)
     }
 
-    // 초기 권한 + GPS 상태 설정
+    // 권한 런처
     private fun initPermissionLauncher() {
-        if (checkLocationService()){
-            locationPermissionRequest =
-                registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
-                    if (isGranted) {
-                        // 권한이 있는 상태
-                        val lm : LocationManager = context?.getSystemService(Context.LOCATION_SERVICE) as LocationManager
-                        val userNowLocation: Location? = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
-                        mCurrentLat = userNowLocation?.latitude // 위도
-                        mCurrentLng = userNowLocation?.longitude // 경도
-                        setCurrentLocation()
+        locationPermissionRequest =
+            registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { }
+    }
 
-                    } else {
-                        checkPermission()
-                    }
-                }
-        } else {
-            // GPS가 꺼져있을 경우
-            Toast.makeText(context, "GPS를 켜주세요.", Toast.LENGTH_SHORT).show()
+    // 사용자 위치 받기
+    private fun startLocationUpdates() {
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
+
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+            && ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return
         }
+
+        fusedLocationClient!!.getCurrentLocation(Priority.PRIORITY_HIGH_ACCURACY, object : CancellationToken() {
+                override fun onCanceledRequested(p0: OnTokenCanceledListener) = CancellationTokenSource().token
+                override fun isCancellationRequested() = false
+
+            }).addOnSuccessListener { location: Location? ->
+                if (location != null) {
+                    mCurrentLat = location.latitude
+                    mCurrentLng = location.longitude
+                    Log.d("위도경도", "${mCurrentLat}, ${mCurrentLng}")
+
+                    setCurrentLocation()
+                }
+            }
     }
 
     // 권한 체크
-    private fun checkPermission() {
-        val isFirstCheck = prefs.getBoolean("isFistImgPermissionCheck", true)
+    private fun checkPermissionForLocation() {
+        val isFirstCheck = prefs.getBoolean("isFistLocationPermissionCheck", true)
+
         when {
-            ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED -> {
-                // 권한이 있는 상태
-                val lm : LocationManager = context?.getSystemService(Context.LOCATION_SERVICE) as LocationManager
-                val userNowLocation: Location? = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
-                mCurrentLat = userNowLocation?.latitude // 위도
-                mCurrentLng = userNowLocation?.longitude // 경도
-                setCurrentLocation()
+            ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED -> {
+                startLocationUpdates()
             }
+
             shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) -> {
                 showFirstPermissionDialog()
             }
+
             else -> {
                 if (isFirstCheck) {
-                    prefs.edit().putBoolean("isFistImgPermissionCheck", false).apply()
-                    locationPermissionRequest.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+                    prefs.edit().putBoolean("isFistLocationPermissionCheck", false).apply()
+                    locationPermissionRequest.launch(
+                        arrayOf(
+                            Manifest.permission.ACCESS_COARSE_LOCATION,
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                        )
+                    )
                 } else {
                     showSecondPermissionDialog()
                 }
@@ -108,21 +129,28 @@ class FindFragment : BaseFragment<FragmentFindBinding>(R.layout.fragment_find) {
     }
 
     private fun showFirstPermissionDialog() {
-        androidx.appcompat.app.AlertDialog.Builder(requireContext())
+        AlertDialog.Builder(requireContext())
             .setMessage("앱 실행을 위해서는 권한을 설정해야 합니다")
             .setPositiveButton("확인") { _, _ ->
-                locationPermissionRequest.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+                locationPermissionRequest.launch(
+                    arrayOf(
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    )
+                )
             }
             .show()
     }
 
     private fun showSecondPermissionDialog() {
-        val builder = androidx.appcompat.app.AlertDialog.Builder(requireContext())
-        builder.setMessage("설정에서 위치 권한을 허용해주세요")
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setMessage("설정에서 권한을 허용해주세요")
         builder.setPositiveButton("설정으로 이동하기") { _, _ ->
             val intent = Intent(
-                Settings.ACTION_APPLICATION_DETAILS_SETTINGS
-            ).setData(Uri.parse("package:" + BuildConfig.APPLICATION_ID))
+                Settings.ACTION_APPLICATION_DETAILS_SETTINGS, Uri.fromParts(
+                    "package", requireContext().packageName, null
+                )
+            )
             startActivity(intent)
         }
         builder.show()
@@ -134,26 +162,28 @@ class FindFragment : BaseFragment<FragmentFindBinding>(R.layout.fragment_find) {
         return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
     }
 
-    // 탐색 화면 들어가자 마자 수행
+    // 초기 화면 위치 + 커스텀 마커 설정
     private fun setCurrentLocation() {
         binding.kakaoMapView.currentLocationTrackingMode = MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading
-        setMyLocationMarker()
+        binding.kakaoMapView.setCustomCurrentLocationMarkerTrackingImage(R.drawable.ic_my_location, MapPOIItem.ImageOffset(20, 20))
     }
 
     // 초기 Zoom 세팅
-    private fun setZoomLevel() { binding.kakaoMapView.setZoomLevel(3, true) }
-
-    // 현재 위치 마커 세팅
-    private fun setMyLocationMarker() {
-        binding.kakaoMapView.setCustomCurrentLocationMarkerTrackingImage(R.drawable.ic_my_location, MapPOIItem.ImageOffset(20,20))
+    private fun setZoomLevel() {
+        binding.kakaoMapView.setZoomLevel(3, true)
     }
 
     // 내 주변 탐색 버튼 세팅
     private fun setTrackingBtn() {
         binding.trackingBtn.setOnClickListener {
             if (status) {
-                // 반경 보이면서 주변 강아지 마커들 보이게 하기
-                startTracking()
+                if (checkLocationService()) {
+                    // 반경 보이면서 주변 강아지 마커들 보이게 하기
+                    startTracking()
+                } else {
+                    setAutoLocation()
+                }
+
             } else {
                 stopTracking()
             }
@@ -161,7 +191,7 @@ class FindFragment : BaseFragment<FragmentFindBinding>(R.layout.fragment_find) {
     }
 
     // 현재 위치 반경 세팅
-    private fun setCircle(circleName : String) {
+    private fun setCircle(circleName: String) {
         val circle1 = MapCircle(
             MapPoint.mapPointWithGeoCoord(mCurrentLat!!, mCurrentLng!!),  // center
             3000,  // radius
@@ -174,16 +204,13 @@ class FindFragment : BaseFragment<FragmentFindBinding>(R.layout.fragment_find) {
         val mapPointBounds = MapPointBounds(mapPointBoundsArray)
         val padding = 50 // px
 
-        when(circleName) {
+        when (circleName) {
             "start" -> {
                 binding.kakaoMapView.addCircle(circle1)
                 binding.kakaoMapView.moveCamera(CameraUpdateFactory.newMapPointBounds(mapPointBounds, padding))
             }
             "end" -> {
                 binding.kakaoMapView.removeAllCircles()
-                binding.kakaoMapView.moveCamera(CameraUpdateFactory.newMapPoint(MapPoint.mapPointWithGeoCoord(mCurrentLat!!, mCurrentLng!!)))
-            }
-            "current" -> {
                 binding.kakaoMapView.moveCamera(CameraUpdateFactory.newMapPoint(MapPoint.mapPointWithGeoCoord(mCurrentLat!!, mCurrentLng!!)))
             }
         }
@@ -208,7 +235,41 @@ class FindFragment : BaseFragment<FragmentFindBinding>(R.layout.fragment_find) {
 
     private fun catchCurrentLocation() {
         binding.fabCurrentLocation.setOnClickListener {
-            setCircle("current")
+            if (checkLocationService()) {
+                binding.kakaoMapView.moveCamera(CameraUpdateFactory.newMapPoint(MapPoint.mapPointWithGeoCoord(mCurrentLat!!, mCurrentLng!!)))
+            } else {
+                setAutoLocation()
+            }
+        }
+    }
+
+    private fun setAutoLocation() {
+        val locationRequest = LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 2000).apply {
+            setGranularity(Granularity.GRANULARITY_PERMISSION_LEVEL)
+            setWaitForAccurateLocation(true)
+        }.build()
+
+        val builder = LocationSettingsRequest.Builder().addLocationRequest(locationRequest)
+        val client: SettingsClient = LocationServices.getSettingsClient(requireContext())
+        val task: Task<LocationSettingsResponse> = client.checkLocationSettings(builder.build())
+
+        task.addOnFailureListener { exception ->
+            // GPS가 꺼져있을 경우
+            binding.kakaoMapView.currentLocationTrackingMode = MapView.CurrentLocationTrackingMode.TrackingModeOff
+            binding.kakaoMapView.setShowCurrentLocationMarker(false)
+            
+            if (exception is ResolvableApiException) {
+                Log.d(TAG, "OnFailure")
+                try {
+                    exception.startResolutionForResult(requireActivity(), 100)
+                } catch (sendEx: IntentSender.SendIntentException) {
+                    Log.d(TAG, sendEx.message.toString())
+                }
+            }
+        }
+
+        task.addOnSuccessListener {
+            checkPermissionForLocation()
         }
     }
 }
