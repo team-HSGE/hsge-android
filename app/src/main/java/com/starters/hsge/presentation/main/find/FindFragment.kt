b08df5dd620edaed210d547ab88c05b89a91582a
syb8200 <syb8200@naver.com>
2022-12-27 11:54:45 +0900

    [FEAT] #113 권한 버전 처리 및 로직 변경

--- app/src/main/java/com/starters/hsge/presentation/main/find/FindFragment.kt
+++ app/src/main/java/com/starters/hsge/presentation/main/find/FindFragment.kt
@@ -1,6 +1,7 @@
 package com.starters.hsge.presentation.main.find
 
 import android.Manifest
+import android.app.Activity
 import android.app.AlertDialog
 import android.content.Context
 import android.content.Intent
@@ -14,109 +15,117 @@ import android.provider.Settings
 import android.util.Log
 import android.view.View
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.lifecycleScope
+import com.bumptech.glide.Glide
+import com.starters.hsge.BuildConfig
 import com.starters.hsge.R
 import com.starters.hsge.databinding.FragmentFindBinding
 import com.starters.hsge.presentation.common.base.BaseFragment
+import kotlinx.coroutines.launch
 import net.daum.mf.map.api.*
 
 class FindFragment : BaseFragment<FragmentFindBinding>(R.layout.fragment_find) {
 
+    private lateinit var locationPermissionRequest: ActivityResultLauncher<String>
+
     private var mCurrentLat : Double? = 0.0
     private var mCurrentLng : Double? = 0.0
-
     private var status : Boolean = true
-    private val ACCESS_FINE_LOCATION = 1000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        initPermissionLauncher()
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        checkPermission()
         setZoomLevel()
         setTrackingBtn()
+        catchCurrentLocation()
     }
 
     override fun onStop() {
         super.onStop()
-        stopTracking()
+
+        binding.kakaoMapView.currentLocationTrackingMode = MapView.CurrentLocationTrackingMode.TrackingModeOff
+        binding.kakaoMapView.setShowCurrentLocationMarker(false)
     }
 
-    private fun setZoomLevel() { binding.kakaoMapView.setZoomLevel(3, true) }
+    // 초기 권한 + GPS 상태 설정
+    private fun initPermissionLauncher() {
+        if (checkLocationService()){
+            locationPermissionRequest =
+                registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+                    if (isGranted) {
+                        // 권한이 있는 상태
+                        val lm : LocationManager = context?.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+                        val userNowLocation: Location? = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
+                        mCurrentLat = userNowLocation?.latitude // 위도
+                        mCurrentLng = userNowLocation?.longitude // 경도
+                        setCurrentLocation()
 
-    // 내 주변 탐색 버튼 세팅
-    private fun setTrackingBtn() {
-        binding.trackingBtn.setOnClickListener {
-            if (status == true) {
-                if (checkLocationService()) {
-                    // GPS가 켜져있을 경우
-                    permissionCheck()
-
-                } else {
-                    // GPS가 꺼져있을 경우
-                    Toast.makeText(context, "GPS를 켜주세요.", Toast.LENGTH_SHORT).show()
+                    } else {
+                        checkPermission()
+                    }
                 }
-
-            } else {
-                stopTracking()
-            }
+        } else {
+            // GPS가 꺼져있을 경우
+            Toast.makeText(context, "GPS를 켜주세요.", Toast.LENGTH_SHORT).show()
         }
     }
 
     // 권한 체크
-    private fun permissionCheck() {
-        val isFirstCheck = prefs.getBoolean("isFirstPermissionCheck", true)
-
-        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // 권한이 없는 상태
-            if (ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(), Manifest.permission.ACCESS_FINE_LOCATION)) {
-                // 권한 거절 (다시 한 번 물어봄)
-                val builder = AlertDialog.Builder(context)
-                builder.setMessage("현재 위치를 확인하시려면 위치 권한을 허용해주세요.")
-                builder.setPositiveButton("확인") { dialog, which ->
-                    ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), ACCESS_FINE_LOCATION)
-                }
-                builder.show()
-            } else {
+    private fun checkPermission() {
+        val isFirstCheck = prefs.getBoolean("isFistImgPermissionCheck", true)
+        when {
+            ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED -> {
+                // 권한이 있는 상태
+                val lm : LocationManager = context?.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+                val userNowLocation: Location? = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
+                mCurrentLat = userNowLocation?.latitude // 위도
+                mCurrentLng = userNowLocation?.longitude // 경도
+                setCurrentLocation()
+            }
+            shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) -> {
+                showFirstPermissionDialog()
+            }
+            else -> {
                 if (isFirstCheck) {
-                    // 최초 권한 요청
-                    prefs.edit().putBoolean("isFirstPermissionCheck", false).apply()
-                    ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), ACCESS_FINE_LOCATION)
+                    prefs.edit().putBoolean("isFistImgPermissionCheck", false).apply()
+                    locationPermissionRequest.launch(Manifest.permission.ACCESS_FINE_LOCATION)
                 } else {
-                    // 다시 묻지 않음 클릭 (앱 정보 화면으로 이동)
-                    val builder = AlertDialog.Builder(context)
-                    builder.setMessage("현재 위치를 확인하시려면 설정에서 위치 권한을 허용해주세요.")
-                    builder.setPositiveButton("설정으로 이동하기") { dialog, which ->
-                        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, Uri.parse("package:" + context?.packageName))
-                        startActivity(intent)
-                    }
-                    builder.show()
+                    showSecondPermissionDialog()
                 }
             }
-        } else {
-            // 권한이 있는 상태
-            val lm : LocationManager = context?.getSystemService(Context.LOCATION_SERVICE) as LocationManager
-            val userNowLocation: Location? = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
-            mCurrentLat = userNowLocation?.latitude // 위도
-            mCurrentLng = userNowLocation?.longitude // 경도
-
-            startTracking()
         }
     }
 
-    // 권한 요청에 따른 분기 처리
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == ACCESS_FINE_LOCATION) {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // 권한 요청 후 승인 됨 (추적 시작)
-                Toast.makeText(context, "위치 권한이 승인되었습니다.", Toast.LENGTH_SHORT).show()
-                startTracking()
-            } else {
-                // 권한 요청 후 거절됨 (다시 요청 or 토스트)
-                Toast.makeText(context, "위치 권한이 거절되었습니다.", Toast.LENGTH_SHORT).show()
-                permissionCheck()
+    private fun showFirstPermissionDialog() {
+        androidx.appcompat.app.AlertDialog.Builder(requireContext())
+            .setMessage("앱 실행을 위해서는 권한을 설정해야 합니다")
+            .setPositiveButton("확인") { _, _ ->
+                locationPermissionRequest.launch(Manifest.permission.ACCESS_FINE_LOCATION)
             }
+            .show()
+    }
+
+    private fun showSecondPermissionDialog() {
+        val builder = androidx.appcompat.app.AlertDialog.Builder(requireContext())
+        builder.setMessage("설정에서 위치 권한을 허용해주세요")
+        builder.setPositiveButton("설정으로 이동하기") { _, _ ->
+            val intent = Intent(
+                Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+            ).setData(Uri.parse("package:" + BuildConfig.APPLICATION_ID))
+            startActivity(intent)
         }
+        builder.show()
     }
 
     // GPS가 켜져있는지 확인
@@ -125,9 +134,30 @@ class FindFragment : BaseFragment<FragmentFindBinding>(R.layout.fragment_find) {
         return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
     }
 
+    // 탐색 화면 들어가자 마자 수행
+    private fun setCurrentLocation() {
+        binding.kakaoMapView.currentLocationTrackingMode = MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading
+        setMyLocationMarker()
+    }
+
+    // 초기 Zoom 세팅
+    private fun setZoomLevel() { binding.kakaoMapView.setZoomLevel(3, true) }
+
     // 현재 위치 마커 세팅
     private fun setMyLocationMarker() {
         binding.kakaoMapView.setCustomCurrentLocationMarkerTrackingImage(R.drawable.ic_my_location, MapPOIItem.ImageOffset(20,20))
+    }
+
+    // 내 주변 탐색 버튼 세팅
+    private fun setTrackingBtn() {
+        binding.trackingBtn.setOnClickListener {
+            if (status) {
+                // 반경 보이면서 주변 강아지 마커들 보이게 하기
+                startTracking()
+            } else {
+                stopTracking()
+            }
+        }
     }
 
     // 현재 위치 반경 세팅
@@ -154,8 +184,6 @@ class FindFragment : BaseFragment<FragmentFindBinding>(R.layout.fragment_find) {
                 binding.kakaoMapView.moveCamera(CameraUpdateFactory.newMapPoint(MapPoint.mapPointWithGeoCoord(mCurrentLat!!, mCurrentLng!!)))
             }
             "current" -> {
-                binding.kakaoMapView.removeAllCircles()
-                binding.kakaoMapView.addCircle(circle1)
                 binding.kakaoMapView.moveCamera(CameraUpdateFactory.newMapPoint(MapPoint.mapPointWithGeoCoord(mCurrentLat!!, mCurrentLng!!)))
             }
         }
@@ -163,28 +191,17 @@ class FindFragment : BaseFragment<FragmentFindBinding>(R.layout.fragment_find) {
 
     // Tracking 상태
     private fun startTracking() {
-        binding.kakaoMapView.currentLocationTrackingMode = MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading
-        setMyLocationMarker()
         setCircle("start")
-
         binding.trackingBtn.setBackgroundResource(R.drawable.bg_dark_yellow_r12)
         binding.trackingBtn.text = "탐색 종료"
-
-        binding.fabCurrentLocation.isClickable = true
-        catchCurrentLocation()
         status = false
         Log.d("추적", "시작")
     }
 
     private fun stopTracking() {
-        binding.kakaoMapView.currentLocationTrackingMode = MapView.CurrentLocationTrackingMode.TrackingModeOff
-        binding.kakaoMapView.setShowCurrentLocationMarker(false)
         setCircle("end")
-
         binding.trackingBtn.setBackgroundResource(R.drawable.bg_yellow_r12)
         binding.trackingBtn.text = "내 주변 탐색"
-
-        binding.fabCurrentLocation.isClickable = false
         status = true
         Log.d("추적", "멈춤")
     }
