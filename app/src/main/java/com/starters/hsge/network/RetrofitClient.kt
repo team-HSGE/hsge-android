a469b57c7abcf10483ffee65e80875cb6b61740c
seoyoon513 <syoonlee513@gmail.com>
2022-12-28 12:07:15 +0900

    [CHORE] #121 base url 변경

--- app/src/main/java/com/starters/hsge/network/RetrofitClient.kt
+++ app/src/main/java/com/starters/hsge/network/RetrofitClient.kt
@@ -12,7 +12,7 @@ object RetrofitClient {
 
     //Retrofit.Builder 클래스의 인스턴스를 만들고 Base URL을 구성해준다.
     val sRetrofit = initRetrofit()
-    private const val AccessToken_URL = "https://dev.hsge.site/"
+    private const val AccessToken_URL = "http://192.168.0.8:8081/"
 
     private fun initRetrofit() : Retrofit =
         Retrofit.Builder()
--- app/src/main/java/com/starters/hsge/presentation/common/Const.kt
+++ app/src/main/java/com/starters/hsge/presentation/common/Const.kt
@@ -1,3 +1,3 @@
 package com.starters.hsge.presentation.common
 
-const val BASE_URL = "https://dev.hsge.site/"
+const val BASE_URL = "http://192.168.0.8:8081/"
