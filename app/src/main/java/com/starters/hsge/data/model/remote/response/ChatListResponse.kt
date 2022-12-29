cf99cb9df2a36e0906aee2f24025f456bc8176a5
seoyoon513 <syoonlee513@gmail.com>
2022-12-29 13:49:04 +0900

    [CHORE] #127 Parcelize 어노테이션 추가

--- app/src/main/java/com/starters/hsge/data/model/remote/response/ChatListResponse.kt
+++ app/src/main/java/com/starters/hsge/data/model/remote/response/ChatListResponse.kt
@@ -1,16 +1,20 @@
 package com.starters.hsge.data.model.remote.response
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
+@Parcelize
 data class ChatListResponse(
     @SerialName("nickname") val nickname: String,
     @SerialName("profilePath") val userIcon: Int,
     @SerialName("latestMessage") val message: String,
     @SerialName("checked") val checked: Boolean, // 메세지 확인 여부
     @SerialName("active") val active: Boolean, // 대화 시작된 채팅방(하단 목록)
+    @SerialName("roomId") val roomId: Long
 
 
     // val date: Int
-)
+): Parcelable
