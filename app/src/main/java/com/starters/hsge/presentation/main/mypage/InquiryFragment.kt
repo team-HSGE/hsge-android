7d4e1c5f8727103c820dfbe43020c35a7c589bee
안정은 <jjeee1313@gmail.com>
2023-01-04 18:45:09 +0900

    [UI] #165 1:1 문의

---
+++ app/src/main/java/com/starters/hsge/presentation/main/mypage/InquiryFragment.kt
@@ -0,0 +1,23 @@
+package com.starters.hsge.presentation.main.mypage
+
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
+import com.starters.hsge.R
+import com.starters.hsge.databinding.FragmentInquiryBinding
+import com.starters.hsge.presentation.common.base.BaseFragment
+
+class InquiryFragment : BaseFragment<FragmentInquiryBinding>(R.layout.fragment_inquiry){
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setNavigation()
+    }
+
+    private fun setNavigation() {
+        binding.toolBar.setNavigationOnClickListener {
+            findNavController().navigateUp()
+        }
+    }
+}
