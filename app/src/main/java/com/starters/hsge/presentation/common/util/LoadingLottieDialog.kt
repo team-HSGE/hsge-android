bf413d052d20b9647711873649728486e73e89ac
안정은 <jjeee1313@gmail.com>
2023-01-04 11:44:30 +0900

    [FEAT] #157 object 생성

---
+++ app/src/main/java/com/starters/hsge/presentation/common/util/LoadingLottieDialog.kt
@@ -0,0 +1,30 @@
+package com.starters.hsge.presentation.common.util
+
+import android.app.Dialog
+import android.content.Context
+import android.graphics.Color
+import androidx.core.graphics.drawable.toDrawable
+import com.starters.hsge.R
+
+object LoadingLottieDialog {
+
+    private lateinit var dogLoadingDialog: Dialog
+
+    fun showDogLoadingDialog(context: Context): Dialog{
+        dogLoadingDialog = Dialog(context)
+        dogLoadingDialog.let {
+            it.show()
+            it.window?.setBackgroundDrawable(Color.TRANSPARENT.toDrawable())
+            it.setContentView(R.layout.dialog_loading_dog)
+            it.setCancelable(false)
+            it.setCanceledOnTouchOutside(false)
+            return it
+        }
+    }
+
+    fun dismissDogLoadingDialog(context: Context){
+        if(dogLoadingDialog.isShowing){
+            dogLoadingDialog.dismiss()
+        }
+    }
+}
