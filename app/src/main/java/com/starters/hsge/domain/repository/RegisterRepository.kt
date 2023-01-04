d7c36d445ebc21bcd79bb913165576a2f3451411
seoyoon513 <syoonlee513@gmail.com>
2023-01-04 14:53:18 +0900

    [CHORE] #162 회원가입 서버통신 응답 flow -> Response로 변경

--- app/src/main/java/com/starters/hsge/domain/repository/RegisterRepository.kt
+++ app/src/main/java/com/starters/hsge/domain/repository/RegisterRepository.kt
@@ -1,11 +1,10 @@
 package com.starters.hsge.domain.repository
 
-import com.starters.hsge.ApiResult
 import com.starters.hsge.data.model.remote.response.SignUpResponse
 import com.starters.hsge.domain.model.RegisterInfo
-import kotlinx.coroutines.flow.Flow
+import retrofit2.Response
 import java.io.File
 
 interface RegisterRepository {
-    suspend fun postUserData(img: File, data: RegisterInfo): Flow<ApiResult<SignUpResponse>>
+    suspend fun postUserData(img: File, data: RegisterInfo): Response<SignUpResponse>
 }
