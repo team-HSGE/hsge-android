1523149585205c9a0f09f65e73299e778f162891
syb8200 <syb8200@naver.com>
2023-01-05 21:44:23 +0900

    [FIX] #178 Request 추가 및 API 통신 관련된 부분 수정

--- app/src/main/java/com/starters/hsge/data/service/SettingsService.kt
+++ app/src/main/java/com/starters/hsge/data/service/SettingsService.kt
@@ -2,6 +2,7 @@ package com.starters.hsge.data.service
 
 import com.starters.hsge.data.api.LoginApi
 import com.starters.hsge.data.interfaces.SettingsInterface
+import com.starters.hsge.data.model.remote.request.FcmPostRequest
 import com.starters.hsge.network.RetrofitClient
 import retrofit2.Call
 import retrofit2.Callback
@@ -9,9 +10,9 @@ import retrofit2.Response
 
 class SettingsService(val settingsInterface: SettingsInterface) {
 
-    fun tryDeleteFcmToken(){
+    fun tryDeleteFcmToken(token: FcmPostRequest){
         val fcmDeleteApi = RetrofitClient.sRetrofit.create(LoginApi::class.java)
-        fcmDeleteApi.deleteFcmToken().enqueue(object :
+        fcmDeleteApi.deleteFcmToken(token).enqueue(object :
             Callback<Void> {
             override fun onResponse(call: Call<Void>, response: Response<Void>) {
                 settingsInterface.onDeleteFcmTokenSuccess(response.isSuccessful)
