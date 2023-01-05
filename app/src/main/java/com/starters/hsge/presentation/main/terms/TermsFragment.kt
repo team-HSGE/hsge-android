c073b3f50acc0fce0192817e8c85dc5319bfa9b9
seoyoon513 <syoonlee513@gmail.com>
2023-01-05 16:27:14 +0900

    [FEAT] #173 textView의 일부 text 색깔 변경

--- app/src/main/java/com/starters/hsge/presentation/main/terms/TermsFragment.kt
+++ app/src/main/java/com/starters/hsge/presentation/main/terms/TermsFragment.kt
@@ -1,6 +1,10 @@
 package com.starters.hsge.presentation.main.terms
 
+import android.graphics.Color
 import android.os.Bundle
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
 import android.view.View
 import com.starters.hsge.R
 import com.starters.hsge.databinding.FragmentTermsBinding
@@ -12,5 +16,17 @@ class TermsFragment : BaseFragment<FragmentTermsBinding>(R.layout.fragment_terms
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        changeTextColor()
+    }
+
+    private fun changeTextColor() {
+        val textData: String = binding.termsGuideSecondLine.text.toString()
+        val builder = SpannableStringBuilder(textData)
+
+        // 색깔 적용
+        val colorBlueSpan = ForegroundColorSpan(Color.parseColor("#FFBF43"))
+        builder.setSpan(colorBlueSpan, 0, 8, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        binding.termsGuideSecondLine.text = builder
     }
 }
