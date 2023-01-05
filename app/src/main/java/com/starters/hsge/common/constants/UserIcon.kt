144fe3345b1cb53af80f906abb88223315af588e
seoyoon513 <syoonlee513@gmail.com>
2023-01-06 06:23:36 +0900

    [FEAT] #183 사용자 아이콘 enum class로 만들기

---
+++ app/src/main/java/com/starters/hsge/common/constants/UserIcon.kt
@@ -0,0 +1,22 @@
+package com.starters.hsge.common.constants
+
+import com.starters.hsge.R
+
+enum class UserIcon (val resId: Int) {
+    ICON_FIRST(R.drawable.ic_dog_profile_1),
+    ICON_SECOND(R.drawable.ic_dog_profile_2),
+    ICON_THIRD(R.drawable.ic_dog_profile_3),
+    ICON_FORTH(R.drawable.ic_dog_profile_4),
+    ICON_FIFTH(R.drawable.ic_dog_profile_5),
+    ICON_SIXTH(R.drawable.ic_dog_profile_6),
+    ICON_SEVENTH(R.drawable.ic_dog_profile_7),
+    ICON_EIGHTH(R.drawable.ic_dog_profile_8),
+    ICON_NINTH(R.drawable.ic_dog_profile_9),
+    ICON_TENTH(R.drawable.ic_dog_profile_10),
+    ICON_ELEVENTH(R.drawable.ic_dog_profile_11),
+    ICON_TWELFTH(R.drawable.ic_dog_profile_12),
+    ICON_THIRTEENTH(R.drawable.ic_dog_profile_13),
+    ICON_FOURTEENTH(R.drawable.ic_dog_profile_14),
+    ICON_FIFTEENTH(R.drawable.ic_dog_profile_15)
+}
+
