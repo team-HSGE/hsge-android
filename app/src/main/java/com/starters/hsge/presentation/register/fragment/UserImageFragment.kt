d8bdaf86ec1bcfcd27c42ade9d85fbcc6f274e5c
seoyoon513 <syoonlee513@gmail.com>
2023-01-06 19:45:02 +0900

    [FEAT] #183 사용자가 선택한 아이콘 띄우기 & 버튼 활성화 처리

--- app/src/main/java/com/starters/hsge/presentation/register/fragment/UserImageFragment.kt
+++ app/src/main/java/com/starters/hsge/presentation/register/fragment/UserImageFragment.kt
@@ -1,51 +1,62 @@
 package com.starters.hsge.presentation.register.fragment
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import androidx.navigation.Navigation
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.starters.hsge.R
 import com.starters.hsge.databinding.FragmentUserImageBinding
 import com.starters.hsge.presentation.common.base.BaseFragment
+import com.starters.hsge.presentation.register.viewmodel.RegisterViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
 
+@AndroidEntryPoint
 class UserImageFragment : BaseFragment<FragmentUserImageBinding>(R.layout.fragment_user_image) {
 
-    var resId : Int? = null
+    var resId: Int? = null
+    var resIdForView: Int? = null
+
+    private val registerViewModel: RegisterViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         initListener()
-        setNavigation()
         getSharedPreferences()
-        if (prefs.contains("resId")){
+        updateUserIcon()
+        setNavigation()
+        changeButtonState()
+    }
+
+    private fun changeButtonState() {
+        if (prefs.contains("resId")) {
             binding.btnNext.isEnabled = true
-            binding.ivUserImage.setImageResource(resId!!)
-        } else {
-            return
+        }
+    }
+
+    private fun updateUserIcon() {
+        if (resIdForView != 0) {
+            resIdForView?.let { binding.ivUserImage.setImageResource(it) }
         }
     }
 
     private fun initListener() {
         binding.btnNext.setOnClickListener {
-            if(prefs.contains("resId")) {
-                Navigation.findNavController(binding.root)
-                    .navigate(R.id.action_userImageFragment_to_dogPhotoFragment)
-            } else {
-                return@setOnClickListener
-            }
+            findNavController().navigate(R.id.action_userImageFragment_to_dogPhotoFragment)
         }
 
         binding.ivUserImage.setOnClickListener {
-            val action = UserImageFragmentDirections.actionUserImageFragmentToUserProfileIconFragment(1)
+            val action =
+                UserImageFragmentDirections.actionUserImageFragmentToUserProfileIconFragment(1)
             findNavController().navigate(action)
         }
     }
 
     private fun getSharedPreferences() {
         resId = prefs.getInt("resId", R.drawable.ic_profile_photo_bg)
-        Log.d("sp", "${resId}")
+        resIdForView = prefs.getInt("resIdForView", 0)
+        Timber.d("선택한 이미지: ${resIdForView}, 매핑: $resId")
     }
 
     private fun setNavigation() {
@@ -53,5 +64,4 @@ class UserImageFragment : BaseFragment<FragmentUserImageBinding>(R.layout.fragme
             findNavController().navigateUp()
         }
     }
-
 }
