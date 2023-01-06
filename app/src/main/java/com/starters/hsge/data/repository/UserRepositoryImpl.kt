4f85c6471f3324ca7cdc002fcc3ea993ade7ce32
seoyoon513 <syoonlee513@gmail.com>
2023-01-06 19:52:24 +0900

    [FEAT] #183 repository 추가, 반환값 flow로 설정, mapping

--- app/src/main/java/com/starters/hsge/data/repository/UserRepositoryImpl.kt
+++ app/src/main/java/com/starters/hsge/data/repository/UserRepositoryImpl.kt
@@ -1,15 +1,29 @@
 package com.starters.hsge.data.repository
 
 import com.starters.hsge.data.api.UserApi
+import com.starters.hsge.data.model.remote.request.EditUserRequest
 import com.starters.hsge.data.model.remote.request.UserLocationRequest
 import com.starters.hsge.di.RetrofitHSGE
+import com.starters.hsge.domain.mapper.mapToUserInfo
+import com.starters.hsge.domain.model.UserInfo
 import com.starters.hsge.domain.repository.UserRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import retrofit2.Response
 import javax.inject.Inject
 
 class UserRepositoryImpl @Inject constructor(
     @RetrofitHSGE private val api: UserApi
 ) : UserRepository {
+    override suspend fun getUserInfo(): Flow<UserInfo> =
+        flow {
+            emit(api.getUserInfo().mapToUserInfo())
+        }
+
+    override suspend fun putUserInfo(userInfo: EditUserRequest): Response<Void> {
+        return api.putUserInfo(userInfo)
+    }
+
     override suspend fun putLocation(location: UserLocationRequest): Response<Void> {
         return api.putLocationData(location)
     }
