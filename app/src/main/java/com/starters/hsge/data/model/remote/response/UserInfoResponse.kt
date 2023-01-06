473341086241cf0754898eb572552287ab6d8ea7
seoyoon513 <syoonlee513@gmail.com>
2023-01-06 19:49:21 +0900

    [FEAT] #183 유저 정보 get 모델, 유저 정보 수정 put 모델 수정

--- app/src/main/java/com/starters/hsge/data/model/remote/response/UserInfoResponse.kt
+++ app/src/main/java/com/starters/hsge/data/model/remote/response/UserInfoResponse.kt
@@ -1,11 +1,16 @@
 package com.starters.hsge.data.model.remote.response
 
-@kotlinx.serialization.Serializable
-data class UserInfoGetResponse(
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+import kotlinx.serialization.Serializable
+
+@Serializable
+@Parcelize
+data class UserInfoResponse(
     val latitude: Double,
     val longtitude: Double,
     val nickname: String,
     val profilePath: Int,
     val town: String,
     val radius: Double
-)
+): Parcelable
