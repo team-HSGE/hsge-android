f175a5949622e82aa894aa6230bc688ae02405f7
seoyoon513 <syoonlee513@gmail.com>
2023-01-06 16:42:08 +0900

    [FEAT] #183 사용자 선택 아이콘 두가지 타입으로 저장 (view, 서버 전송)

--- app/src/main/java/com/starters/hsge/presentation/register/fragment/UserProfileIconFragment.kt
+++ app/src/main/java/com/starters/hsge/presentation/register/fragment/UserProfileIconFragment.kt
@@ -2,66 +2,89 @@ package com.starters.hsge.presentation.register.fragment
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.viewModels
 import androidx.navigation.NavDirections
-import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.starters.hsge.R
+import com.starters.hsge.common.constants.UserIcon
 import com.starters.hsge.databinding.FragmentUserProfileIconBinding
 import com.starters.hsge.presentation.common.base.BaseFragment
+import com.starters.hsge.presentation.common.util.UserIconFormat.applyUserIconToInt
 import com.starters.hsge.presentation.register.adapter.UserProfileIconAdapter
+import com.starters.hsge.presentation.register.viewmodel.RegisterViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
-class UserProfileIconFragment : BaseFragment<FragmentUserProfileIconBinding>(R.layout.fragment_user_profile_icon) {
+@AndroidEntryPoint
+class UserProfileIconFragment :
+    BaseFragment<FragmentUserProfileIconBinding>(R.layout.fragment_user_profile_icon) {
+
+    private val registerViewModel: RegisterViewModel by viewModels()
 
     private lateinit var adapter: UserProfileIconAdapter
-    private val args : UserProfileIconFragmentArgs by navArgs()
-    private lateinit var action : NavDirections
+    private val args: UserProfileIconFragmentArgs by navArgs()
+    private lateinit var action: NavDirections
 
-    var userIconList = listOf<Int>()
+    var userIconList = listOf<UserIcon>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        userIconList = listOf<Int>(
-            R.drawable.ic_dog_profile_1,
-            R.drawable.ic_dog_profile_6,
-            R.drawable.ic_dog_profile_11,
-            R.drawable.ic_dog_profile_2,
-            R.drawable.ic_dog_profile_7,
-            R.drawable.ic_dog_profile_12,
-            R.drawable.ic_dog_profile_3,
-            R.drawable.ic_dog_profile_8,
-            R.drawable.ic_dog_profile_13,
-            R.drawable.ic_dog_profile_4,
-            R.drawable.ic_dog_profile_9,
-            R.drawable.ic_dog_profile_14,
-            R.drawable.ic_dog_profile_5,
-            R.drawable.ic_dog_profile_10,
-            R.drawable.ic_dog_profile_15
+        userIconList = listOf(
+            UserIcon.ICON_FIRST,
+            UserIcon.ICON_SECOND,
+            UserIcon.ICON_THIRD,
+            UserIcon.ICON_FORTH,
+            UserIcon.ICON_FIFTH,
+            UserIcon.ICON_SIXTH,
+            UserIcon.ICON_SEVENTH,
+            UserIcon.ICON_EIGHTH,
+            UserIcon.ICON_NINTH,
+            UserIcon.ICON_TENTH,
+            UserIcon.ICON_ELEVENTH,
+            UserIcon.ICON_TWELFTH,
+            UserIcon.ICON_THIRTEENTH,
+            UserIcon.ICON_FOURTEENTH,
+            UserIcon.ICON_FIFTEENTH
         )
 
         setNavigation()
         initRecyclerView(userIconList)
     }
 
-    private fun initRecyclerView(list: List<Int>) {
-        adapter = UserProfileIconAdapter(list)
-        binding.rvProfileIcon.adapter = adapter
+    private fun initRecyclerView(list: List<UserIcon>) {
+        adapter = UserProfileIconAdapter(list, itemClickListener = {
+            if (args.checkLayout == 1) { // register 화면
+                // SharedPreference 저장
+                prefs.edit().putInt("resId", applyUserIconToInt(it)).apply()
+                prefs.edit().putInt("resIdForView", it.resId).apply()
 
-        adapter.setItemClickListener(object: UserProfileIconAdapter.OnItemClickListener{
-            override fun onClick(v: View, resId: Int) {
-                if(args.checkLayout == 1) {
-                    // 회원가입
-                    prefs.edit().putInt("resId", resId).apply()
-                    Navigation.findNavController(binding.root)
-                        .navigate(R.id.action_userProfileIconFragment_to_userImageFragment)
-                } else if (args.checkLayout == 2) {
-                    // 회원 정보 수정
-                    action = UserProfileIconFragmentDirections.actionUserProfileEditIconFragmentToUserProfileEditFragment(null, resId)
-                    findNavController().navigate(action)
+                findNavController().navigate(
+                    R.id.action_userProfileIconFragment_to_userImageFragment
+                )
+
+                /* 데이터 스토어에 저장 시 delay를 주지 않으면 저장이 안되는 문제 있음
+                lifecycleScope.launch(Dispatchers.Main) {
+                    val job = lifecycleScope.launch(Dispatchers.Main) {
+                        registerViewModel.saveUserIcon(applyUserIconToInt(it)) // mapping
+                        registerViewModel.saveUserIconForView(it.resId)
+                        Timber.d("유저 아이콘 ${registerViewModel.fetchUserIcon().first()}, ${it.resId}")
+                    }
+                    job.join()
                 }
+                 */
+
+            } else if (args.checkLayout == 2) { // 유저 정보 수정 화면
+                //safe args로 전달
+                action =
+                    UserProfileIconFragmentDirections.actionUserProfileEditIconFragmentToUserProfileEditFragment(
+                        null,
+                        it.resId
+                    )
+                findNavController().navigate(action)
             }
         })
+        binding.rvProfileIcon.adapter = adapter
     }
 
     private fun setNavigation() {
