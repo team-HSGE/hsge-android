0f2b7b274712a772b790a80edd3fee0f0b778ec1
seoyoon513 <syoonlee513@gmail.com>
2023-01-09 21:23:36 +0900

    [FEAT] #183 chatInfo 모델로 맵핑 & repository 추가

--- app/src/main/java/com/starters/hsge/data/repository/ChatRepositoryImpl.kt
+++ app/src/main/java/com/starters/hsge/data/repository/ChatRepositoryImpl.kt
@@ -3,12 +3,22 @@ package com.starters.hsge.data.repository
 import com.starters.hsge.data.api.ChatApi
 import com.starters.hsge.data.model.remote.response.MessageInfoResponse
 import com.starters.hsge.di.RetrofitHSGE
+import com.starters.hsge.domain.mapper.mapToChatListInfo
+import com.starters.hsge.domain.model.ChatListInfo
 import com.starters.hsge.domain.repository.ChatRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class ChatRepositoryImpl @Inject constructor(
     @RetrofitHSGE private val api: ChatApi
 ) : ChatRepository {
+
+    override suspend fun getChatList(): Flow<List<ChatListInfo>?> = flow {
+        emit(api.getChatList()?.map {
+            it.mapToChatListInfo()
+        })
+    }
 
     override suspend fun getMessageList(roomId: Long): MessageInfoResponse {
         return api.getChatRecord(roomId)
--- app/src/main/java/com/starters/hsge/domain/repository/ChatRepository.kt
+++ app/src/main/java/com/starters/hsge/domain/repository/ChatRepository.kt
@@ -1,8 +1,12 @@
 package com.starters.hsge.domain.repository
 
 import com.starters.hsge.data.model.remote.response.MessageInfoResponse
+import com.starters.hsge.domain.model.ChatListInfo
+import kotlinx.coroutines.flow.Flow
 
 interface ChatRepository {
+
+    suspend fun getChatList(): Flow<List<ChatListInfo>?>
 
     suspend fun getMessageList(roomId: Long): MessageInfoResponse
 
