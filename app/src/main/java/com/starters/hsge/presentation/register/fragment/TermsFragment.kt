9573c3f758a9cc2904c5c360a11ca9ee4f66c4af
안정은 <jjeee1313@gmail.com>
2023-01-10 15:40:34 +0900

    [FEAT] #197 모두 체크 시 버튼 활성화

--- app/src/main/java/com/starters/hsge/presentation/register/fragment/TermsFragment.kt
+++ app/src/main/java/com/starters/hsge/presentation/register/fragment/TermsFragment.kt
@@ -18,6 +18,7 @@ class TermsFragment : BaseFragment<FragmentTermsBinding>(R.layout.fragment_terms
         super.onViewCreated(view, savedInstanceState)
 
         changeTextColor()
+        checkTerms()
     }
 
     private fun changeTextColor() {
@@ -28,5 +29,45 @@ class TermsFragment : BaseFragment<FragmentTermsBinding>(R.layout.fragment_terms
         val colorBlueSpan = ForegroundColorSpan(Color.parseColor("#FFBF43"))
         builder.setSpan(colorBlueSpan, 0, 8, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
         binding.termsGuideSecondLine.text = builder
+    }
+
+    private fun checkTerms(){
+        binding.cbTotalAgree.setOnClickListener(SwitchListener())
+        binding.cbFirstTerm.setOnClickListener(SwitchListener())
+        binding.cbSecondTerm.setOnClickListener(SwitchListener())
+        binding.cbThirdTerm.setOnClickListener(SwitchListener())
+
+    }
+
+    inner class SwitchListener : View.OnClickListener {
+        override fun onClick(v: View?) {
+
+            val totalCheck = binding.cbTotalAgree
+            val firstCheck = binding.cbFirstTerm
+            val secondCheck = binding.cbSecondTerm
+            val thirdCheck = binding.cbThirdTerm
+
+            when (v?.id) {
+                R.id.cb_total_agree -> {
+                    if (totalCheck.isChecked) {
+                        firstCheck.isChecked = true
+                        secondCheck.isChecked = true
+                        thirdCheck.isChecked = true
+                    } else {
+                        firstCheck.isChecked = false
+                        secondCheck.isChecked = false
+                        thirdCheck.isChecked = false
+                    }
+                }
+                R.id.cb_first_term, R.id.cb_second_term, R.id.cb_third_term -> {
+                    totalCheck.isChecked = firstCheck.isChecked && secondCheck.isChecked && thirdCheck.isChecked
+                }
+            }
+            changeAgreeBtn()
+        }
+    }
+
+    private fun changeAgreeBtn(){
+            binding.btnNext.isEnabled = binding.cbTotalAgree.isChecked
     }
 }
