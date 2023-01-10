13d8157dbe1455f2af62a3b967c880b734ce18a1
seoyoon513 <syoonlee513@gmail.com>
2023-01-10 15:15:37 +0900

    [FEAT] #183 fragment에 usecase 주입

--- app/src/main/java/com/starters/hsge/presentation/register/fragment/ProfileIconFragment.kt
+++ app/src/main/java/com/starters/hsge/presentation/register/fragment/ProfileIconFragment.kt
@@ -9,11 +9,13 @@ import com.starters.hsge.common.constants.UserIcon
 import com.starters.hsge.common.constants.iconToOrder
 import com.starters.hsge.databinding.FragmentProfileIconBinding
 import com.starters.hsge.domain.model.UserProfileInfo
+import com.starters.hsge.domain.usecase.GetIconUseCase
 import com.starters.hsge.presentation.common.base.BaseFragment
 import com.starters.hsge.presentation.common.constants.SAVE_RESID_FOR_VIEW
 import com.starters.hsge.presentation.common.constants.SAVE_RESID_ORDER
 import com.starters.hsge.presentation.register.adapter.UserProfileIconAdapter
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class ProfileIconFragment :
@@ -22,35 +24,17 @@ class ProfileIconFragment :
     val REGISTER_SCREEN = 1
     val USER_PROFILE_SCREEN = 2
 
+    @Inject
+    lateinit var getIconUseCase: GetIconUseCase
     private lateinit var adapter: UserProfileIconAdapter
 
     private val args: ProfileIconFragmentArgs by navArgs()
 
-    var userIconList = listOf<UserIcon>()
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        userIconList = listOf(
-            UserIcon.ICON_FIRST,
-            UserIcon.ICON_SECOND,
-            UserIcon.ICON_THIRD,
-            UserIcon.ICON_FORTH,
-            UserIcon.ICON_FIFTH,
-            UserIcon.ICON_SIXTH,
-            UserIcon.ICON_SEVENTH,
-            UserIcon.ICON_EIGHTH,
-            UserIcon.ICON_NINTH,
-            UserIcon.ICON_TENTH,
-            UserIcon.ICON_ELEVENTH,
-            UserIcon.ICON_TWELFTH,
-            UserIcon.ICON_THIRTEENTH,
-            UserIcon.ICON_FOURTEENTH,
-            UserIcon.ICON_FIFTEENTH
-        )
-
         setNavigation()
-        initRecyclerView(userIconList)
+        initRecyclerView(getIconUseCase.invoke())
     }
 
     private fun initRecyclerView(list: List<UserIcon>) {
@@ -69,17 +53,6 @@ class ProfileIconFragment :
                     )
                 findNavController().navigate(action)
             }
-
-            /* 데이터 스토어에 저장 시 delay를 주지 않으면 저장이 안되는 문제 있음
-            lifecycleScope.launch(Dispatchers.Main) {
-                val job = lifecycleScope.launch(Dispatchers.IO) {
-                    registerViewModel.saveUserIcon(applyUserIconToInt(it)) // mapping
-                    registerViewModel.saveUserIconForView(it.resId)
-                    Timber.d("유저 아이콘 ${registerViewModel.fetchUserIcon().first()}, ${it.resId}")
-                }
-                job.join()
-            }
-             */
         })
         binding.rvProfileIcon.adapter = adapter
     }
