0b7367486b48057e80e7d573d2c79a253d605671
seoyoon513 <syoonlee513@gmail.com>
2023-01-10 19:48:34 +0900

    [FEAT] #199 ViewType -> attrId 추가한 TagViewType으로 변경

---
+++ app/src/main/java/com/starters/hsge/common/constants/TagViewType.kt
@@ -0,0 +1,8 @@
+package com.starters.hsge.common.constants
+
+import com.starters.hsge.R
+
+enum class TagViewType(val attrId: Int) {
+    LIKE(R.attr.CustomLikeChipChoiceStyle),
+    DISLIKE(R.attr.CustomDislikeChipChoiceStyle)
+}
