5c350274524e401c18bc4677dccfe15f7ddf01dc
syb8200 <syb8200@naver.com>
2023-01-11 11:48:52 +0900

    [CHORE] #210 온보딩 화면 기본 세팅

---
+++ app/src/main/java/com/starters/hsge/presentation/onboarding/OnBoardingActivity.kt
@@ -0,0 +1,15 @@
+package com.starters.hsge.presentation.onboarding
+
+import android.content.Intent
+import android.os.Bundle
+import com.starters.hsge.R
+import com.starters.hsge.databinding.ActivityMainBinding
+import com.starters.hsge.presentation.common.base.BaseActivity
+import com.starters.hsge.presentation.main.MainActivity
+
+class OnBoardingActivity: BaseActivity<ActivityMainBinding>(R.layout.activity_onboarding) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
