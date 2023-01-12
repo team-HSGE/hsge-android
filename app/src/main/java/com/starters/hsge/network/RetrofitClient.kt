3f8c05dab1a40c6a12d2909a7559fe8692efceea
seoyoon513 <syoonlee513@gmail.com>
2023-01-12 10:35:47 +0900

    [CHORE] #213 로컬 서버 추가

--- app/src/main/java/com/starters/hsge/network/RetrofitClient.kt
+++ app/src/main/java/com/starters/hsge/network/RetrofitClient.kt
@@ -16,6 +16,7 @@ object RetrofitClient {
     val sRetrofit = initRetrofit()
     //private const val AccessToken_URL = "http://192.168.0.8:8081/" // 채팅용
     private const val AccessToken_URL = "https://dev.hsge.site/"
+    //private const val AccessToken_URL = "http://192.168.0.57:8080/" // 태민
     //private const val AccessToken_URL = "http://192.168.0.148:8080/" // 나가기
 
     private fun initRetrofit() : Retrofit =
