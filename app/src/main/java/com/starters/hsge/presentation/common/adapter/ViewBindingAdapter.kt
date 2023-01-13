d81979fb4782caf7d1f3d76f7387e8d34d527570
seoyoon513 <syoonlee513@gmail.com>
2023-01-13 16:46:27 +0900

    [FEAT] #219 채팅방 메세지 '분' 기준으로 시간 표시

--- app/src/main/java/com/starters/hsge/presentation/common/adapter/ViewBindingAdapter.kt
+++ app/src/main/java/com/starters/hsge/presentation/common/adapter/ViewBindingAdapter.kt
@@ -5,6 +5,8 @@ import android.view.View
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.starters.hsge.data.model.remote.response.Message
+import com.starters.hsge.presentation.common.util.DateFormat.changeDayFormat
+import com.starters.hsge.presentation.common.util.DateFormat.changeMinFormat
 import com.starters.hsge.presentation.main.chat.chatroom.ChatRoomViewModel
 import java.text.SimpleDateFormat
 
@@ -15,28 +17,63 @@ fun View.viewVisibility(isNeuter: Boolean) {
             this.visibility = View.GONE
         }
         true -> {
-
         }
     }
 }
 
 @SuppressLint("SimpleDateFormat")
 @BindingAdapter("bindMessage", "bindMessageViewModel")
+fun TextView.bindShouldMessageShowDividerText(message: Message?, viewModel: ChatRoomViewModel) {
+    message?.let {
+        val index = viewModel.messages.indexOf(message)
+
+        if (index == 0) {
+            this.visibility = View.GONE
+        } else {
+            val messageBefore = viewModel.messages[index - 1]
+
+            val dateFormat = SimpleDateFormat("yyyy.MM.dd HH:mm:ss")
+            val beforeDate = dateFormat.parse(changeDayFormat(messageBefore.timeStamp))
+            val afterDate = dateFormat.parse(changeDayFormat(message.timeStamp))
+
+            val diff = (afterDate.time - beforeDate.time) / 1000
+            val diffDay = diff / (60 * 60 * 24)
+
+            if (diffDay > 0) {
+                this.visibility = View.VISIBLE
+            } else {
+                this.visibility = View.GONE
+            }
+        }
+    }
+}
+
+@SuppressLint("SimpleDateFormat")
+@BindingAdapter("bindSingleMessage", "bindSingleMessageViewModel")
 fun TextView.bindShouldMessageShowTimeText(message: Message?, viewModel: ChatRoomViewModel) {
     message?.let {
-        val messageBefore = viewModel.messages[viewModel.messages.size - 1]
-
-        val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
-        val beforeDate = dateFormat.parse(messageBefore.timeStamp)
-        val afterDate = dateFormat.parse(message.timeStamp)
-
-        val diff = (afterDate.time - beforeDate.time) / 1000
-        val diffDay = diff / (60 * 60 * 24)
-
-        if (diffDay > 0) {
+        val index = viewModel.messages.indexOf(message)
+        if (index == 0) {
             this.visibility = View.VISIBLE
         } else {
-            this.visibility = View.GONE
+            val messageBefore = viewModel.messages[index - 1]
+
+            val dateFormat = SimpleDateFormat("yyyy.MM.dd HH:mm:ss")
+            val beforeDate = dateFormat.parse(changeMinFormat(messageBefore.timeStamp))
+            val afterDate = dateFormat.parse(changeMinFormat(message.timeStamp))
+
+            val diff = (afterDate.time - beforeDate.time) / 1000
+            val diffMin = diff / 60
+
+            if (messageBefore.senderId != message.senderId) {
+                this.visibility = View.VISIBLE
+            } else {
+                if (diffMin > 0) {
+                    this.visibility = View.VISIBLE
+                } else {
+                    this.visibility = View.GONE
+                }
+            }
         }
     }
 }
